2371679065af2a92117c03f2fd94ee90c917aba3
krishpranav <krisna.pranav@gmail.com>
2021-03-29 19:14:48 +0530

    done changes

--- fs/fat12.c
+++ fs/fat12.c
@@ -1,29 +1,34 @@
-//includes
-#include "<linklist.h>"
-#include "<error.h>"
+#include <linklist.h>
+#include <error.h>
 #include "fat12.h"
 #include "vfs.h"
 
-
 #define ROOT_DIR_OFFSET 0x2600
-#define ROOT_DIR_SIZE 14 * 512 //14 sectors
+#define ROOT_DIR_SIZE 14 * 512 /* 14 sectors */
 #define MAX_FILES_IN_DIR 224
-#define FAT12_MAX_FILENAME_LENGHT 11
+#define FAT12_MAX_FILENAME_LENGTH 11
 
 struct fat12_bootsector
 {
+    /* The bootsector holds quite a lot of info, but we will have just the parts
+     * which interest us when working with the driver itself */
+
     size_t fat_cnt;
     size_t max_root_dir_cnt;
     size_t max_sector_cnt;
     size_t sectors_in_fat;
 };
 
-struct fat12_moun{
+/* On every mount a strcture is created */
+struct fat12_mount
+{
     struct llist_t ll;
-
+    /* FS driver is just to have some compare value for get_mount_point() */
     struct fs_driver *fs_drv;
     struct fat12_bootsector bootsec;
+    /* Multi-dimensional array of all FAT data. FAT count is in `bootsec` */
     char **fat_data;
+    /* Root directory data */
     char rootdir_data[ROOT_DIR_SIZE];
     size_t open_file_cnt;
 };
@@ -38,7 +43,39 @@ struct rootdir_item
     unsigned short last_access_date;
     unsigned short ignore_in_fat12;
     unsigned short last_write_time;
-    unsigned short last_wrtie_date;
+    unsigned short last_write_date;
     unsigned short fat_idx;
     size_t filesize;
 };
+
+struct fat12_mount *fat12_mounts;
+
+static struct fat12_mount *get_mount_point(struct fs_driver *drv)
+{
+    struct fat12_mount *mount;
+    size_t idx;
+
+    llist_foreach(fat12_mounts, mount, idx, ll)
+    {
+        if (mount->fs_drv == drv)
+            return mount;
+    }
+
+    return NULL;
+}
+
+static int init_bootsec(struct dev_driver *dev, struct fat12_bootsector *bootsec)
+{
+    char data[512];
+
+    dev->read(data, 0, 512);
+    if (!data)
+        return -1;
+
+    bootsec->fat_cnt = data[16];
+    bootsec->max_root_dir_cnt = ((data[18] << 8) & 0xFF00) | (data[17] & 0xFF);
+    bootsec->max_sector_cnt = ((data[20] << 8) & 0xFF00) | (data[19] & 0xFF);
+    bootsec->sectors_in_fat = ((data[23] << 8) & 0xFF00) | (data[22] & 0xFF);
+
+    return 0;
+}
