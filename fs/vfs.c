8f66ac8c8fb8ce0d0df8d054ce1382695257ac51
krishpranav <krisna.pranav@gmail.com>
2021-03-30 16:35:19 +0530

    done changes

--- fs/vfs.c
+++ fs/vfs.c
@@ -30,3 +30,50 @@ int vfs_init()
 
     return 0;
 }
+
+int mount(enum storage_dev_type dev_type, char *filename)
+{
+    struct mount_point *mount;
+
+    if (!filename)
+        return -EBADARG;
+
+    /* Create a new mount_point */
+    mount = (struct mount_point *) kalloc(sizeof(struct mount_point));
+    if (!mount)
+        return -ENOMEM;
+    mount->fs_driver = (struct fs_driver *) kalloc(sizeof(struct fs_driver));
+    if (!mount->fs_driver)
+        return -ENOMEM;
+    mount->fs_driver->dev_driver = (struct dev_driver *) kalloc(sizeof(struct dev_driver));
+    if (!mount->fs_driver->dev_driver)
+        return -ENOMEM;
+    mount->name = (char *) kalloc(MAX_MOUNTNAME_SIZE);
+    strcpy(mount->name, filename);
+
+    /* Initialize the storage driver functions */
+    switch (dev_type) {
+        case STORAGE_DEVICE_FLOPPY:
+            floppy_init_driver(mount->fs_driver->dev_driver);
+            fat12_init_fs(mount->fs_driver);
+            break;
+        case STORAGE_DEVICE_HDD:
+            printf("HDD storage devices are not implemented yet");
+            break;
+        default:
+            return -EBADARG;
+    }
+
+    /* Add the mount point the the VFS directory pool */
+    if (_vfs.mount_pts)
+        llist_add_before(_vfs.mount_pts, mount, ll);
+    else
+    {
+        llist_init(mount, ll);
+        _vfs.mount_pts = mount;
+    }
+    _vfs.dir_count++;
+
+    return 0;
+}
+
