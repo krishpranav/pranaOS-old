70dae80537c37a82d965c684128026d825d9eca1
krishpranav <krisna.pranav@gmail.com>
2021-03-30 16:49:43 +0530

    done change

--- libc/libc.c
+++ libc/libc.c
@@ -15,3 +15,31 @@ static inline void put_tab();
 
 #define CHAR_TO_MEMVAL(chr) \
         ((chr) | (_color << 8))
+
+void clear_screen()
+{
+    int i;
+    int max_offset = MAX_CRS_X * MAX_CRS_Y;
+
+    for (i = 0; i < max_offset; i++)
+    {
+        short mem_val = CHAR_TO_MEMVAL(' ');
+        ((short *)VIDEO_MEMORY)[i] = mem_val;
+    }
+}
+
+void set_color(unsigned char backgrnd, unsigned char forgrnd)
+{
+    _color = (forgrnd | (backgrnd << 4));
+}
+
+void goto_xy(unsigned x, unsigned y)
+{
+    _cursor_loc = MAX_CRS_X * y + x;
+}
+
+void cursor_save()
+{
+    _cursor_save = _cursor_loc;
+    _color_save = _color;
+}
