c12e33ba4cf9e8ca7889adc8ad956e5ce1415a77
krishpranav <krisna.pranav@gmail.com>
2021-03-31 12:42:51 +0530

    done changes

---
+++ kernel32/mm.h
@@ -0,0 +1,22 @@
+#ifndef MM_ZPVRK7R1
+#define MM_ZPVRK7R1
+
+#include <libc.h>
+
+struct boot_info {
+    unsigned int mem_size;
+    unsigned int krnl_size;
+    unsigned int krnl_loc;
+} __attribute__((__packed__));
+
+/* PMM */
+addr_t pmm_init(unsigned int mem_kb, addr_t bitmap_loc);
+int pmm_init_region(unsigned int addr, size_t size);
+extern void *pmm_alloc(unsigned int bytes);
+size_t get_total_mem_b();
+size_t get_free_mem_b();
+size_t get_used_mem_b();
+size_t get_krnl_size();
+
+
+
