f6c48cc59fd2a89ef462b0989fca9a3a71b8fc25
krishpranav <krisna.pranav@gmail.com>
2021-04-09 13:02:23 +0530

    done changes

---
+++ AK/Badge.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace AK {
+
+template<typename T>
+class Badge {
+public:
+    using Type = T;
+
+private:
+    friend T;
+    constexpr Badge() = default;
+
+    Badge(const Badge&) = delete;
+    Badge& operator=(const Badge&) = delete;
+
+    Badge(Badge&&) = delete;
+    Badge& operator=(Badge&&) = delete;
+};
+
+}
+
+using AK::Badge;
