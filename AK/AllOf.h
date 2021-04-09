37de0a8621f1f8ac8539d12691d97cd613d3fb27
krishpranav <krisna.pranav@gmail.com>
2021-04-09 12:58:57 +0530

    done changes and adding files

--- AK/AllOf.h
+++ AK/AllOf.h
@@ -4,19 +4,19 @@
 
 namespace AK {
 
-    template<typename Container, typename ValueType>
-    constexpr bool all_of(
-            const SimpleIterator<Container, ValueType>& begin,
-            const SimpleIterator<Container, ValueType>& end,
-            const auto& predicate)
-    {
-        for (auto iter = begin; iter != end; ++iter) {
-            if (!predicate(*iter)) {
-                return false;
-            }
+template<typename Container, typename ValueType>
+constexpr bool all_of(
+    const SimpleIterator<Container, ValueType>& begin,
+    const SimpleIterator<Container, ValueType>& end,
+    const auto& predicate)
+{
+    for (auto iter = begin; iter != end; ++iter) {
+        if (!predicate(*iter)) {
+            return false;
         }
-        return true;
     }
+    return true;
+}
 
 }
 
--- AK/AnyOf.h
+++ AK/AnyOf.h
@@ -1,23 +1,22 @@
-
 #pragma once
 
 #include <AK/Iterator.h>
 
 namespace AK {
 
-    template<typename Container, typename ValueType>
-    constexpr bool any_of(
-            const SimpleIterator<Container, ValueType>& begin,
-            const SimpleIterator<Container, ValueType>& end,
-            const auto& predicate)
-    {
-        for (auto iter = begin; iter != end; ++iter) {
-            if (predicate(*iter)) {
-                return true;
-            }
+template<typename Container, typename ValueType>
+constexpr bool any_of(
+    const SimpleIterator<Container, ValueType>& begin,
+    const SimpleIterator<Container, ValueType>& end,
+    const auto& predicate)
+{
+    for (auto iter = begin; iter != end; ++iter) {
+        if (predicate(*iter)) {
+            return true;
         }
-        return false;
     }
+    return false;
+}
 
 }
 
