d69dd781da19498729a52db8f3224ab15e3a2c49
krishpranav <krisna.pranav@gmail.com>
2021-04-09 19:40:44 +0530

    done changes

---
+++ AK/BitCast.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace AK {
+
+template<typename T, typename U>
+inline T bit_cast(const U& a)
+{
+    static_assert(sizeof(T) == sizeof(U));
+
+    T result;
+    __builtin_memcpy(&result, &a, sizeof(T));
+    return result;
+}
+
+}
+
+using AK::bit_cast;
