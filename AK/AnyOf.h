936249b4bba5d05783cf80b6a8ae7508d0c83536
krishpranav <krisna.pranav@gmail.com>
2021-04-10 11:59:17 +0530

    done changes

--- AK/AnyOf.h
+++ AK/AnyOf.h
@@ -1,6 +1,4 @@
 /*
- * Copyright (c) 2021, the SerenityOS developers.
- * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
--- AK/BinaryHeap.h
+++ AK/BinaryHeap.h
@@ -1,6 +1,4 @@
 /*
- * Copyright (c) 2021, Idan Horowitz <idan.horowitz@gmail.com>
- * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
