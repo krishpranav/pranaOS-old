a65476a23c66744df2a398ff730e971488de83fc
krishpranav <krisna.pranav@gmail.com>
2021-04-28 16:57:05 +0530

    updated address sanitize h file updated

---
+++ Kernel/AddressSanitizer.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// includes
+#include <AK/Types.h>
+
+namespace Kernel::AddressSanitizer {
+
+void shadow_va_check_load(unsigned long address, size_t size, void* return_addr);
+
+void shadow_va_check_store(unsigned long address, size_t size, void* return_addr);
+
+}
