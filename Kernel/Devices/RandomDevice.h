096e4ce29cf491129a783f9a39d0ffda25823a51
krishpranav <krisna.pranav@gmail.com>
2021-05-04 10:27:16 +0530

    adding files, RandomDevice cpp, h file updated

--- Kernel/Devices/RandomDevice.h
+++ Kernel/Devices/RandomDevice.h
@@ -1,30 +1,26 @@
 #pragma once
-
 // includes
 #include "CharacterDevice.h"
 
 namespace Kernel {
 
-class NullDevice final : public CharacterDevice {
+class RandomDevice final : public CharacterDevice {
     AK_MAKE_ETERNAL
 public:
-    NullDevice();
-    virtual ~NullDevice() override;
-
-    static void initialize();
-    static NullDevice& the();
+    RandomDevice();
+    virtual ~RandomDevice() override;
 
     // ^Device
     virtual mode_t required_mode() const override { return 0666; }
-    virtual String device_name() const override { return "null"; }
+    virtual String device_name() const override { return "random"; }
 
 private:
     // ^CharacterDevice
     virtual KResultOr<size_t> read(FileDescription&, u64, UserOrKernelBuffer&, size_t) override;
     virtual KResultOr<size_t> write(FileDescription&, u64, const UserOrKernelBuffer&, size_t) override;
-    virtual bool can_write(const FileDescription&, size_t) const override { return true; }
     virtual bool can_read(const FileDescription&, size_t) const override;
-    virtual const char* class_name() const override { return "NullDevice"; }
+    virtual bool can_write(const FileDescription&, size_t) const override { return true; }
+    virtual const char* class_name() const override { return "RandomDevice"; }
 };
 
 }
