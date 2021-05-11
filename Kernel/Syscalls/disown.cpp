6b803a4dc2e6f0106b02fb7cbbd8afcc74ff9762
krishpranav <krisna.pranav@gmail.com>
2021-05-11 12:22:38 +0530

    update disown cpp

---
+++ Kernel/Syscalls/disown.cpp
@@ -0,0 +1,19 @@
+// includes
+#include <Kernel/Process.h>
+
+namespace Kernel {
+
+KResultOr<int> Process::sys$disown(ProcessID pid)
+{
+    REQUIRE_PROMISE(proc);
+    auto process = Process::from_pid(pid);
+    if (!process)
+        return ESRCH;
+    if (process->ppid() != this->pid())
+        return ECHILD;
+    ProtectedDataMutationScope scope(*process);
+    process->m_ppid = 0;
+    process->disowned_by_waiter(*this);
+    return 0;
+}
+}
