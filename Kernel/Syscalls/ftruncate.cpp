204a65e7c375ece7d80fe1dcbf05ff6a98de4685
krishpranav <krisna.pranav@gmail.com>
2021-05-11 12:27:06 +0530

    update ftruncate functions

---
+++ Kernel/Syscalls/ftruncate.cpp
@@ -0,0 +1,23 @@
+// includes
+#include <Kernel/FileSystem/FileDescription.h>
+#include <Kernel/Process.h>
+
+namespace Kernel {
+
+KResultOr<int> Process::sys$ftruncate(int fd, Userspace<off_t*> userspace_length)
+{
+    REQUIRE_PROMISE(stdio);
+    off_t length;
+    if (!copy_from_user(&length, userspace_length))
+        return EFAULT;
+    if (length < 0)
+        return EINVAL;
+    auto description = file_description(fd);
+    if (!description)
+        return EBADF;
+    if (!description->is_writable())
+        return EBADF;
+    return description->truncate(static_cast<u64>(length));
+}
+
+}
