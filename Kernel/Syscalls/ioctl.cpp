db3cb40c69fcb9758ab89004f3441ab1ecddd2bd
krishpranav <krisna.pranav@gmail.com>
2021-05-11 12:30:36 +0530

    update functions for ioctl

---
+++ Kernel/Syscalls/ioctl.cpp
@@ -0,0 +1,20 @@
+// includes
+#include <Kernel/FileSystem/FileDescription.h>
+#include <Kernel/Process.h>
+#include <LibC/sys/ioctl_numbers.h>
+
+namespace Kernel {
+
+KResultOr<int> Process::sys$ioctl(int fd, unsigned request, FlatPtr arg)
+{
+    auto description = file_description(fd);
+    if (!description)
+        return EBADF;
+    if (request == FIONBIO) {
+        description->set_blocking(arg == 0);
+        return KSuccess;
+    }
+    return description->file().ioctl(*description, request, arg);
+}
+
+}
