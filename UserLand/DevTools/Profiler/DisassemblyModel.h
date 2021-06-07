9d57883b88640b31dbcf926beacce276e2371612
krishpranav <krisna.pranav@gmail.com>
2021-06-07 16:28:55 +0530

    [+] DisassemblyModel header file updated [+]

--- UserLand/DevTools/Profiler/DisassemblyModel.h
+++ UserLand/DevTools/Profiler/DisassemblyModel.h
@@ -25,5 +25,38 @@ struct InstructionData {
     float percent { 0 };
 };
 
+class DisassemblyModel final : public GUI::Model {
+public:
+    static NonnullRefPtr<DisassemblyModel> create(Profile& profile, ProfileNode& node)
+    {
+        return adopt_ref(*new DisassemblyModel(profile, node));
+    }
+
+    enum Column {
+        Address,
+        SampleCount,
+        InstructionBytes,
+        Disassembly,
+        __Count
+    };
+
+    virtual ~DisassemblyModel() override;
+
+    virtual int row_count(const GUI::ModelIndex& = GUI::ModelIndex()) const override;
+    virtual int column_count(const GUI::ModelIndex& = GUI::ModelIndex()) const override { return Column::__Count; }
+    virtual String column_name(int) const override;
+    virtual GUI::Variant data(const GUI::ModelIndex&, GUI::ModelRole) const override;
+    virtual void update() override;
+    virtual bool is_column_sortable(int) const override { return false; }
+
+private:
+    DisassemblyModel(Profile&, ProfileNode&);
+
+    Profile& m_profile;
+    ProfileNode& m_node;
+    RefPtr<MappedFile> m_kernel_file;
+
+    Vector<InstructionData> m_instructions;
+};
 
 }
