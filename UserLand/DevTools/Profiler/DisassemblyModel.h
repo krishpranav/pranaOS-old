036b0f17f4114af088940122f4d5c216779e6f05
krishpranav <krisna.pranav@gmail.com>
2021-06-07 16:27:12 +0530

    [+] Dis assembly Model header includes update [+]

---
+++ UserLand/DevTools/Profiler/DisassemblyModel.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+
+#pragma once
+
+// includes
+#include <LibGUI/Model.h>
+#include <LibX86/Instruction.h>
