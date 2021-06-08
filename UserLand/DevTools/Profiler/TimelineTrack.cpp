5c3df749dfa75b66ff75ba35f7be1dff336b940f
krishpranav <krisna.pranav@gmail.com>
2021-06-08 16:59:32 +0530

    [+] Time line track [+]

--- UserLand/DevTools/Profiler/TimelineTrack.cpp
+++ UserLand/DevTools/Profiler/TimelineTrack.cpp
@@ -15,4 +15,16 @@
 
 namespace Profiler {
 
+TimelineTrack::TimelineTrack(TimelineView const& view, Profile const& profile, Process const& process)
+    : m_view(view)
+    , m_profile(profile)
+    , m_process(process)
+{
+    set_fill_with_background_color(true);
+    set_background_role(Gfx::ColorRole::Base);
+    set_fixed_height(40);
+    set_scale(view.scale());
+    set_frame_thickness(1);
+}
+
 }
