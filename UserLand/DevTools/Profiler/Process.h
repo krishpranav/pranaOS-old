880ccbc3b3f507f860178b05ef5f3eefcdef347b
krishpranav <krisna.pranav@gmail.com>
2021-06-08 09:09:59 +0530

    [+] Process header update [+]

---
+++ UserLand/DevTools/Profiler/Process.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include "EventSerialNumber.h"
+#include <AK/HashMap.h>
+#include <AK/MappedFile.h>
+#include <AK/OwnPtr.h>
+#include <AK/Vector.h>
+#include <LibELF/Image.h>
