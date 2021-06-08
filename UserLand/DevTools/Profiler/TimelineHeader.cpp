0e34f86a9644c05ba0da3af19538299716ecd21e
krishpranav <krisna.pranav@gmail.com>
2021-06-08 16:52:02 +0530

    [+] includes [+]

--- UserLand/DevTools/Profiler/TimelineHeader.cpp
+++ UserLand/DevTools/Profiler/TimelineHeader.cpp
@@ -3,3 +3,14 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
 */
+
+// includes
+#include "TimelineHeader.h"
+#include "Process.h"
+#include "Profile.h"
+#include <AK/LexicalPath.h>
+#include <LibGUI/FileIconProvider.h>
+#include <LibGUI/Icon.h>
+#include <LibGUI/Painter.h>
+#include <LibGfx/Font.h>
+#include <LibGfx/Palette.h>
