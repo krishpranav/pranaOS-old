a25780f25282af16c6e34ffa7a2011d75e88bb58
krishpranav <krisna.pranav@gmail.com>
2021-06-08 16:37:06 +0530

    [+] namespace profiler, classes added [+]

---
+++ UserLand/DevTools/Profiler/TimelineHeader.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include <LibGUI/Frame.h>
+
+namespace Profiler {
+
+class Profile;
+class Process;
+
+}
