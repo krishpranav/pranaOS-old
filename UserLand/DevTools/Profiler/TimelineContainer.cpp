031df560636d85e9eee4aec5dc49b7dc09768a4c
krishpranav <krisna.pranav@gmail.com>
2021-06-08 16:42:48 +0530

    [+] Time line container incldes updated [+]

---
+++ UserLand/DevTools/Profiler/TimelineContainer.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include "TimelineContainer.h"
+#include "TimelineView.h"
+#include <LibGUI/Layout.h>
