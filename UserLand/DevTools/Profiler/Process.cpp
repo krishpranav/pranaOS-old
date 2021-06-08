0ccb2a6af9e9204231583224e2fe0461b51ca125
krishpranav <krisna.pranav@gmail.com>
2021-06-08 10:34:26 +0530

    [+] Thread find thread [+]

--- UserLand/DevTools/Profiler/Process.cpp
+++ UserLand/DevTools/Profiler/Process.cpp
@@ -10,7 +10,14 @@ namespace Profiler {
 
 Thread* Process::find_thread(pid_t tid, EventSerialNumber serial)
 {
-    
+    auto it = threads.find(tid);
+    if (it == threads.end())
+        return nullptr;
+    for (auto& thread : it->value) {
+        if (thread.start_valid < serial && (thread.end_valid == EventSerialNumber {} || thread.end_valid > serial))
+            return &thread;
+    }
+    return nullptr;
 }
 
 }
