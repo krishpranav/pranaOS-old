fbfe87e5d5e78f4b7d2dc26f0d02d79a4e1e6878
krishpranav <krisna.pranav@gmail.com>
2021-06-08 11:28:50 +0530

    [+] Profile Model includes [+]

---
+++ UserLand/DevTools/Profiler/ProfileModel.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+// includes
+#include "ProfileModel.h"
+#include "Profile.h"
+#include <AK/StringBuilder.h>
+#include <LibGUI/FileIconProvider.h>
+#include <ctype.h>
+#include <stdio.h>
