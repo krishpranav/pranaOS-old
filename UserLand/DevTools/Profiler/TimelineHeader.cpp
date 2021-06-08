c8d362c9db75d13ef42ac6fb1ded0a5253cc70a5
krishpranav <krisna.pranav@gmail.com>
2021-06-08 16:52:59 +0530

    [+] Time line header [+]

--- UserLand/DevTools/Profiler/TimelineHeader.cpp
+++ UserLand/DevTools/Profiler/TimelineHeader.cpp
@@ -14,3 +14,21 @@
 #include <LibGUI/Painter.h>
 #include <LibGfx/Font.h>
 #include <LibGfx/Palette.h>
+
+namespace Profiler {
+
+
+TimelineHeader::TimelineHeader(Profile& profile, Process const& process)
+    : m_profile(profile)
+    , m_process(process)
+{
+    set_frame_shape(Gfx::FrameShape::Panel);
+    set_frame_shadow(Gfx::FrameShadow::Raised);
+    set_fixed_size(200, 40);
+    update_selection();
+
+    m_icon = GUI::FileIconProvider::icon_for_executable(m_process.executable).bitmap_for_size(32);
+    m_text = String::formatted("{} ({})", LexicalPath(m_process.executable).basename(), m_process.pid);
+}
+
+}
