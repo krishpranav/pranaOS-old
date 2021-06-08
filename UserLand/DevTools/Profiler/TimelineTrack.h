2c5476c4313e46cb03b6a43a344351741672b22d
krishpranav <krisna.pranav@gmail.com>
2021-06-08 16:58:02 +0530

    [+] virtual public, private functions [+]

--- UserLand/DevTools/Profiler/TimelineTrack.h
+++ UserLand/DevTools/Profiler/TimelineTrack.h
@@ -10,9 +10,27 @@
 
 namespace Profiler {
 
-
 class Process;
 class Profile;
 class TimelineView;
+
+class TimelineTrack final : public GUI::Frame {
+    C_OBJECT(TimelineTrack);
+
+public:
+    virtual ~TimelineTrack() override;
+
+    void set_scale(float);
+
+private:
+    virtual void event(Core::Event&) override;
+    virtual void paint_event(GUI::PaintEvent&) override;
+
+    explicit TimelineTrack(TimelineView const&, Profile const&, Process const&);
+
+    TimelineView const& m_view;
+    Profile const& m_profile;
+    Process const& m_process;
+};
 
 }
