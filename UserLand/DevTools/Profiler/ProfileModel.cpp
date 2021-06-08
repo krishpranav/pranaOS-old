9123547679eb88695d759272cce29f9d952b62a7
krishpranav <krisna.pranav@gmail.com>
2021-06-08 11:32:27 +0530

    [+] neated the code [+]

--- UserLand/DevTools/Profiler/ProfileModel.cpp
+++ UserLand/DevTools/Profiler/ProfileModel.cpp
@@ -12,13 +12,14 @@
 #include <ctype.h>
 #include <stdio.h>
 
-namespace  Profiler {
+namespace Profiler {
 
-    ProfileModel::ProfileModel(Profile& profile)
-            : m_profile(profile)
-    {
-        m_user_frame_icon.set_bitmap_for_size(16, Gfx::Bitmap::load_from_file("/res/icons/16x16/inspector-object.png"));
-        m_kernel_frame_icon.set_bitmap_for_size(16, Gfx::Bitmap::load_from_file("/res/icons/16x16/inspector-object-red.png"));
-    }
+ProfileModel::ProfileModel(Profile& profile)
+    : m_profile(profile)
+{
+    m_user_frame_icon.set_bitmap_for_size(16, Gfx::Bitmap::load_from_file("/res/icons/16x16/inspector-object.png"));
+    m_kernel_frame_icon.set_bitmap_for_size(16, Gfx::Bitmap::load_from_file("/res/icons/16x16/inspector-object-red.png"));
+}
+
 
 }
