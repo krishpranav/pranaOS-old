d7d12a2016e38c5955e4ae47d32823d98fdde3be
krishpranav <krisna.pranav@gmail.com>
2021-06-08 17:11:24 +0530

    [+] Time line view includes [+]

---
+++ UserLand/DevTools/Profiler/TimelineView.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include "TimelineView.h"
+#include "Profile.h"
+#include "TimelineTrack.h"
+#include <LibGUI/BoxLayout.h>
