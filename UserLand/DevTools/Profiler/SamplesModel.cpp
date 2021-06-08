87b9baf0f81cd4d6a6325cd86bd3641da9f8e558
krishpranav <krisna.pranav@gmail.com>
2021-06-08 15:34:37 +0530

    [+] includes [+]

---
+++ UserLand/DevTools/Profiler/SamplesModel.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include "SamplesModel.h"
+#include "Profile.h"
+#include <AK/StringBuilder.h>
+#include <stdio.h>
