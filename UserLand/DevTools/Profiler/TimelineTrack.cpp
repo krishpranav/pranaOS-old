1ef85c130ca7cf7775a4cea9fb6fcc56feeeb178
krishpranav <krisna.pranav@gmail.com>
2021-06-08 16:58:27 +0530

    [+] includes [+]

---
+++ UserLand/DevTools/Profiler/TimelineTrack.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include "TimelineTrack.h"
+#include "Histogram.h"
+#include "Profile.h"
+#include "TimelineView.h"
+#include <LibGUI/Painter.h>
+#include <LibGfx/Font.h>
+#include <LibGfx/Palette.h>
