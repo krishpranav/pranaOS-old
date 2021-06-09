e781ad856f421adbf7131d612d8a12806cc082c2
krishpranav <krisna.pranav@gmail.com>
2021-06-09 09:23:56 +0530

    [+] includes [+]

---
+++ UserLand/DevTools/StateMachineGenerator/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+// includes
+#include <AK/GenericLexer.h>
+#include <AK/HashTable.h>
+#include <AK/OwnPtr.h>
+#include <AK/SourceGenerator.h>
+#include <AK/String.h>
+#include <AK/StringBuilder.h>
+#include <AK/Types.h>
+#include <LibCore/ArgsParser.h>
+#include <LibCore/File.h>
+#include <ctype.h>
+#include <string.h>
