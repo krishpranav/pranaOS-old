f5d545eaceefbfd1b1c183638f12f24dbc795965
krishpranav <krisna.pranav@gmail.com>
2021-06-11 14:16:50 +0530

    [+] Added Range Allocator [+]

--- UserLand/DevTools/UserspaceEmulator/RangeAllocator.cpp
+++ UserLand/DevTools/UserspaceEmulator/RangeAllocator.cpp
@@ -11,8 +11,7 @@
 #include <AK/Random.h>
 
 #define VM_GUARD_PAGES
-#define PAGE ((FlatPtr)0xfffff000u)
-
+#define PAGE_MASK ((FlatPtr)0xfffff000u)
 
 namespace UserspaceEmulator {
 
@@ -47,5 +46,140 @@ void RangeAllocator::carve_at_index(int index, const Range& range)
     }
 }
 
+Optional<Range> RangeAllocator::allocate_randomized(size_t size, size_t alignment)
+{
+    if (!size)
+        return {};
+
+    VERIFY((size % PAGE_SIZE) == 0);
+    VERIFY((alignment % PAGE_SIZE) == 0);
+
+    // FIXME: I'm sure there's a smarter way to do this.
+    static constexpr size_t maximum_randomization_attempts = 1000;
+    for (size_t i = 0; i < maximum_randomization_attempts; ++i) {
+        VirtualAddress random_address { round_up_to_power_of_two(get_random<FlatPtr>(), alignment) };
+
+        if (!m_total_range.contains(random_address, size))
+            continue;
+
+        auto range = allocate_specific(random_address, size);
+        if (range.has_value())
+            return range;
+    }
+
+    return allocate_anywhere(size, alignment);
+}
+
+Optional<Range> RangeAllocator::allocate_anywhere(size_t size, size_t alignment)
+{
+    if (!size)
+        return {};
+
+    VERIFY((size % PAGE_SIZE) == 0);
+    VERIFY((alignment % PAGE_SIZE) == 0);
+
+#ifdef VM_GUARD_PAGES
+    // NOTE: We pad VM allocations with a guard page on each side.
+    if (Checked<size_t>::addition_would_overflow(size, PAGE_SIZE * 2))
+        return {};
+
+    size_t effective_size = size + PAGE_SIZE * 2;
+    size_t offset_from_effective_base = PAGE_SIZE;
+#else
+    size_t effective_size = size;
+    size_t offset_from_effective_base = 0;
+#endif
+
+    if (Checked<size_t>::addition_would_overflow(effective_size, alignment))
+        return {};
+
+    for (size_t i = 0; i < m_available_ranges.size(); ++i) {
+        auto& available_range = m_available_ranges[i];
+        // FIXME: This check is probably excluding some valid candidates when using a large alignment.
+        if (available_range.size() < (effective_size + alignment))
+            continue;
+
+        FlatPtr initial_base = available_range.base().offset(offset_from_effective_base).get();
+        FlatPtr aligned_base = round_up_to_power_of_two(initial_base, alignment);
+
+        Range allocated_range(VirtualAddress(aligned_base), size);
+        VERIFY(m_total_range.contains(allocated_range));
+
+        if (available_range == allocated_range) {
+            m_available_ranges.remove(i);
+            return allocated_range;
+        }
+        carve_at_index(i, allocated_range);
+        return allocated_range;
+    }
+    dbgln("RangeAllocator: Failed to allocate anywhere: size={}, alignment={}", size, alignment);
+    return {};
+}
+
+Optional<Range> RangeAllocator::allocate_specific(VirtualAddress base, size_t size)
+{
+    if (!size)
+        return {};
+
+    VERIFY(base.is_page_aligned());
+    VERIFY((size % PAGE_SIZE) == 0);
+
+    Range allocated_range(base, size);
+    if (!m_total_range.contains(allocated_range)) {
+        dbgln("Unallocatable mmap request?! {:p}+{:p}", base.get(), size);
+        return {};
+    }
+    for (size_t i = 0; i < m_available_ranges.size(); ++i) {
+        auto& available_range = m_available_ranges[i];
+        if (!available_range.contains(base, size))
+            continue;
+        if (available_range == allocated_range) {
+            m_available_ranges.remove(i);
+            return allocated_range;
+        }
+        carve_at_index(i, allocated_range);
+        return allocated_range;
+    }
+    return {};
+}
+
+void RangeAllocator::deallocate(const Range& range)
+{
+    VERIFY(m_total_range.contains(range));
+    VERIFY(range.size());
+    VERIFY((range.size() % PAGE_SIZE) == 0);
+    VERIFY(range.base() < range.end());
+    VERIFY(!m_available_ranges.is_empty());
+
+    size_t nearby_index = 0;
+    auto* existing_range = binary_search(
+        m_available_ranges.span(),
+        range,
+        &nearby_index,
+        [](auto& a, auto& b) { return a.base().get() - b.end().get(); });
+
+    size_t inserted_index = 0;
+    if (existing_range) {
+        existing_range->m_size += range.size();
+        inserted_index = nearby_index;
+    } else {
+        m_available_ranges.insert_before_matching(
+            Range(range), [&](auto& entry) {
+                return entry.base() >= range.end();
+            },
+            nearby_index, &inserted_index);
+    }
+
+    if (inserted_index < (m_available_ranges.size() - 1)) {
+        // We already merged with previous. Try to merge with next.
+        auto& inserted_range = m_available_ranges[inserted_index];
+        auto& next_range = m_available_ranges[inserted_index + 1];
+        if (inserted_range.end() == next_range.base()) {
+            inserted_range.m_size += next_range.size();
+            m_available_ranges.remove(inserted_index + 1);
+            return;
+        }
+    }
+}
 
 }
