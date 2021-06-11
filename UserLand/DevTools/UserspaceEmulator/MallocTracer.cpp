db6aa6ac7357c34ce5811848118a9833da7c5f0c
krishpranav <krisna.pranav@gmail.com>
2021-06-11 11:41:00 +0530

    [+] includes [+]

---
+++ UserLand/DevTools/UserspaceEmulator/MallocTracer.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+// includes
+#include "MmapRegion.h"
+#include "SoftMMU.h"
+#include <AK/Badge.h>
+#include <AK/HashMap.h>
+#include <AK/OwnPtr.h>
+#include <AK/Types.h>
+#include <AK/Vector.h>
