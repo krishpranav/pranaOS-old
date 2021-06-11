09d5edd931ce22be5adbe7f2c746ee6abd2f9602
krishpranav <krisna.pranav@gmail.com>
2021-06-11 13:22:33 +0530

    [+] mmap initialized static function [+]

--- UserLand/DevTools/UserspaceEmulator/MmapRegion.cpp
+++ UserLand/DevTools/UserspaceEmulator/MmapRegion.cpp
@@ -9,3 +9,15 @@
 #include "Emulator.h"
 #include <string.h>
 #include <sys/mman.h>
+
+namespace UserspaceEmulator {
+
+static void* mmap_initialized(size_t bytes, char initial_value, const char* name)
+{
+    auto* ptr = mmap_with_name(nullptr, bytes, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0, name);
+    VERIFY(ptr != MAP_FAILED);
+    memset(ptr, initial_value, bytes);
+    return ptr;
+}
+
+}
