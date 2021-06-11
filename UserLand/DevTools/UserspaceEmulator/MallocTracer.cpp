47777afecf382aa03a553865eea9d5fdf1e3bcfd
krishpranav <krisna.pranav@gmail.com>
2021-06-11 12:58:35 +0530

    [+] includes [+]

--- UserLand/DevTools/UserspaceEmulator/MallocTracer.cpp
+++ UserLand/DevTools/UserspaceEmulator/MallocTracer.cpp
@@ -4,3 +4,12 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+// includes
+#include "MallocTracer.h"
+#include "Emulator.h"
+#include "MmapRegion.h"
+#include <AK/Debug.h>
+#include <AK/TemporaryChange.h>
+#include <mallocdefs.h>
+#include <string.h>
+#include <unistd.h>
