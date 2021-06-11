51041401cff52b2a18e6f9bec5df3f91ec793eb7
krishpranav <krisna.pranav@gmail.com>
2021-06-11 15:28:11 +0530

    [+] Region includes [+]

---
+++ UserLand/DevTools/UserspaceEmulator/Region.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes 
+#include "Range.h"
+#include "ValueWithShadow.h"
+#include <AK/TypeCasts.h>
+#include <AK/Types.h>
+#include <LibX86/Types.h>
