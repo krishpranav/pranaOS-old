668f85f6a5b1953282ecce4d56d959e7271b0a84
krishpranav <krisna.pranav@gmail.com>
2021-06-11 11:00:41 +0530

    [+] includes [+]

---
+++ UserLand/DevTools/UserspaceEmulator/Emulator.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+// incldes
+#include "MallocTracer.h"
+#include "RangeAllocator.h"
+#include "Report.h"
+#include "SoftCPU.h"
+#include "SoftMMU.h"
+#include <AK/MappedFile.h>
+#include <AK/Types.h>
+#include <LibDebug/DebugInfo.h>
+#include <LibELF/AuxiliaryVector.h>
+#include <LibELF/Image.h>
+#include <LibX86/Instruction.h>
+#include <signal.h>
+#include <sys/types.h>
