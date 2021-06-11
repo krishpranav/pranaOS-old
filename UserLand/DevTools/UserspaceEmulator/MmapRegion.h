e099c4f9bb5bf690c628ee8aac3d9fb6df0db83f
krishpranav <krisna.pranav@gmail.com>
2021-06-11 13:18:11 +0530

    [+] public static values [+]

--- UserLand/DevTools/UserspaceEmulator/MmapRegion.h
+++ UserLand/DevTools/UserspaceEmulator/MmapRegion.h
@@ -15,5 +15,13 @@ namespace UserspaceEmulator {
 class MallocRegionMetadata;
 class MallocTracer;
 
+class MmapRegion final : public Region {
+public:
+    static NonnullOwnPtr<MmapRegion> create_anonymous(u32 base, u32 size, u32 prot, String name);
+    static NonnullOwnPtr<MmapRegion> create_file_backed(u32 base, u32 size, u32 prot, int flags, int fd, off_t offset, String name);
+    virtual ~MmapRegion() override;
+
+
+}
 
 }
