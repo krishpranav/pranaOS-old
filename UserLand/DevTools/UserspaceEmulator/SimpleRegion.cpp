eb1707366cd5af6818db15ec3294be1f00abef38
krishpranav <krisna.pranav@gmail.com>
2021-06-11 15:34:21 +0530

    [+] Simple Region [+]

--- UserLand/DevTools/UserspaceEmulator/SimpleRegion.cpp
+++ UserLand/DevTools/UserspaceEmulator/SimpleRegion.cpp
@@ -8,3 +8,16 @@
 // includes
 #include "SimpleRegion.h"
 #include <string.h>
+
+namespace UserspaceEmulator {
+
+
+SimpleRegion::SimpleRegion(u32 base, u32 size)
+    : Region(base, size)
+{
+    m_data = (u8*)calloc(1, size);
+    m_shadow_data = (u8*)malloc(size);
+    memset(m_shadow_data, 1, size);
+}
+
+}
