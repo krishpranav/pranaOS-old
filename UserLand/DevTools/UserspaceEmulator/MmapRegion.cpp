041c58300649aa0ef612243ef91bb3f30740d4b3
krishpranav <krisna.pranav@gmail.com>
2021-06-11 13:20:58 +0530

    [+] includes [+]

---
+++ UserLand/DevTools/UserspaceEmulator/MmapRegion.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include "MmapRegion.h"
+#include "Emulator.h"
+#include <string.h>
+#include <sys/mman.h>
