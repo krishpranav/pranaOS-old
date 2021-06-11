07766792d4a25d7564b2ee37f6c9c9f96fb3a43f
krishpranav <krisna.pranav@gmail.com>
2021-06-11 14:11:57 +0530

    [+] Optional Range [+]

--- UserLand/DevTools/UserspaceEmulator/RangeAllocator.h
+++ UserLand/DevTools/UserspaceEmulator/RangeAllocator.h
@@ -17,5 +17,10 @@ public:
     RangeAllocator();
 
     void initialize_with_range(VirtualAddress, size_t);
+
+    Optional<Range> allocate_anywhere(size_t, size_t alignment = PAGE_SIZE);
+    Optional<Range> allocate_specific(VirtualAddress, size_t);
+    Optional<Range> allocate_randomized(size_t, size_t alignment);
+    
 }
 }
