cad858ae64200dd70790a4cc9462efefc0520139
krishpranav <krisna.pranav@gmail.com>
2021-06-11 13:30:09 +0530

    [+] Range public, private functions updated [+]

--- UserLand/DevTools/UserspaceEmulator/Range.h
+++ UserLand/DevTools/UserspaceEmulator/Range.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-// includes
+// incldues
 #include <AK/Types.h>
 #include <Kernel/VirtualAddress.h>
 
@@ -26,6 +26,51 @@ public:
     VirtualAddress base() const { return m_base; }
     size_t size() const { return m_size; }
     bool is_valid() const { return !m_base.is_null(); }
+
+    bool contains(VirtualAddress vaddr) const { return vaddr >= base() && vaddr < end(); }
+
+    VirtualAddress end() const { return m_base.offset(m_size); }
+
+    bool operator==(const Range& other) const
+    {
+        return m_base == other.m_base && m_size == other.m_size;
+    }
+
+    bool contains(VirtualAddress base, size_t size) const
+    {
+        if (base.offset(size) < base)
+            return false;
+        return base >= m_base && base.offset(size) <= end();
+    }
+
+    bool contains(const Range& other) const
+    {
+        return contains(other.base(), other.size());
+    }
+
+    Vector<Range, 2> carve(const Range&) const;
+
+    Range split_at(VirtualAddress address)
+    {
+        VERIFY(address.is_page_aligned());
+        VERIFY(m_base < address);
+        size_t new_size = (address - m_base).get();
+        VERIFY(new_size < m_size);
+        size_t other_size = m_size - new_size;
+        m_size = new_size;
+        return { address, other_size };
+    }
+
+private:
+    VirtualAddress m_base;
+    size_t m_size { 0 };
+};
+
 }
 
+namespace AK {
+template<>
+struct Traits<UserspaceEmulator::Range> : public GenericTraits<UserspaceEmulator::Range> {
+    static constexpr bool is_trivial() { return true; }
+};
 }
