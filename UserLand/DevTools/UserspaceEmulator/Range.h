ef5c1c2c5f04e0e145a05ef9130ad84acf338c43
krishpranav <krisna.pranav@gmail.com>
2021-06-11 13:28:56 +0530

    [+] base virtual address [+]

--- UserLand/DevTools/UserspaceEmulator/Range.h
+++ UserLand/DevTools/UserspaceEmulator/Range.h
@@ -15,7 +15,13 @@ namespace UserspaceEmulator {
 class Range {
     friend class RangeAllocator;
 
-
+public:
+    Range() = delete;
+    Range(VirtualAddress base, size_t size)
+        : m_base(base)
+        , m_size(size)
+    {
+    }
 }
 
 }
