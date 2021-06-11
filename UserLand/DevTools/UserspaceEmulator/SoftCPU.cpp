992319bb0d8d724fee703ff8550a352a6c0d0a08
krishpranav <krisna.pranav@gmail.com>
2021-06-11 15:44:20 +0530

    [+] includes [+]

---
+++ UserLand/DevTools/UserspaceEmulator/SoftCPU.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include "SoftCPU.h"
+#include "Emulator.h"
+#include <AK/Assertions.h>
+#include <AK/BitCast.h>
+#include <AK/Debug.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
