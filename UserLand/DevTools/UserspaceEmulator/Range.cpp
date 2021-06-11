ea24ada8de040347c9ac7e8ca2943abfdbce8338
krishpranav <krisna.pranav@gmail.com>
2021-06-11 13:31:57 +0530

    [+] Vector range carve updated [+]

--- UserLand/DevTools/UserspaceEmulator/Range.cpp
+++ UserLand/DevTools/UserspaceEmulator/Range.cpp
@@ -14,6 +14,13 @@ Vector<Range, 2> Range::carve(const Range& taken) const
 {
     VERIFY((taken.size() % PAGE_SIZE) == 0);
     Vector<Range, 2> parts;
+    if (taken == *this)
+        return {};
+    if (taken.base() > base())
+        parts.append({ base(), taken.base().get() - base().get() });
+    if (taken.end() < end())
+        parts.append({ taken.end(), end().get() - taken.end().get() });
+    return parts;
 }
 
 }
