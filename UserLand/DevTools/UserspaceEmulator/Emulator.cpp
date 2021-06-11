731f1a9ec5e55e08b73061bb6ee57202b63969c3
krishpranav <krisna.pranav@gmail.com>
2021-06-11 11:16:04 +0530

    [+] includes [+]

---
+++ UserLand/DevTools/UserspaceEmulator/Emulator.cpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+// includes
+#include "Emulator.h"
+#include "MmapRegion.h"
+#include "SimpleRegion.h"
+#include "SoftCPU.h"
+#include <AK/Debug.h>
+#include <AK/Format.h>
+#include <AK/LexicalPath.h>
+#include <AK/MappedFile.h>
+#include <LibELF/AuxiliaryVector.h>
+#include <LibELF/Image.h>
+#include <LibELF/Validation.h>
+#include <LibX86/ELFSymbolProvider.h>
+#include <fcntl.h>
+#include <syscall.h>
+#include <unistd.h>
