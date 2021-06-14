b70e2152f05aae6488d64bca9560f11680501752
krishpranav <krisna.pranav@gmail.com>
2021-06-14 13:20:53 +0530

    [+] const expr [+]

--- UserLand/DevTools/UserspaceEmulator/ValueWithShadow.h
+++ UserLand/DevTools/UserspaceEmulator/ValueWithShadow.h
@@ -11,3 +11,12 @@
 #include <AK/Platform.h>
 #include <LibX86/Types.h>
 #include <string.h>
+
+namespace UserspaceEmulator {
+
+constexpr u64 _inititalized_64 = 0x01010101'01010101LLU;
+constexpr u128 _initialized_128 = u128(_inititalized_64, _inititalized_64);
+constexpr u256 _initialized_256 = u256(_initialized_128, _initialized_128);
+
+
+}
