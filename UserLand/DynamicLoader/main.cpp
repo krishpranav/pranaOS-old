261c6bc0243d3539811311777f55c39f957deab4
krishpranav <krisna.pranav@gmail.com>
2021-06-14 13:35:56 +0530

    [+] perform self relocations [+]

--- UserLand/DynamicLoader/main.cpp
+++ UserLand/DynamicLoader/main.cpp
@@ -20,3 +20,37 @@ static void init_libc()
     __stdio_is_initialized = false;
     __libc_init();
 }
+
+static void perform_self_relocations(auxv_t* auxvp)
+{
+
+    FlatPtr base_address = 0;
+    bool found_base_address = false;
+    for (; auxvp->a_type != AT_NULL; ++auxvp) {
+        if (auxvp->a_type == ELF::AuxiliaryValue::BaseAddress) {
+            base_address = auxvp->a_un.a_val;
+            found_base_address = true;
+        }
+    }
+    VERIFY(found_base_address);
+    Elf32_Ehdr* header = (Elf32_Ehdr*)(base_address);
+    Elf32_Phdr* pheader = (Elf32_Phdr*)(base_address + header->e_phoff);
+    u32 dynamic_section_addr = 0;
+    for (size_t i = 0; i < (size_t)header->e_phnum; ++i, ++pheader) {
+        if (pheader->p_type != PT_DYNAMIC)
+            continue;
+        dynamic_section_addr = pheader->p_vaddr + base_address;
+    }
+    if (!dynamic_section_addr)
+        exit(1);
+
+    auto dynamic_object = ELF::DynamicObject::create({}, (VirtualAddress(base_address)), (VirtualAddress(dynamic_section_addr)));
+
+    dynamic_object->relocation_section().for_each_relocation([base_address](auto& reloc) {
+        if (reloc.type() != R_386_RELATIVE)
+            return IterationDecision::Continue;
+
+        *(u32*)reloc.address().as_ptr() += base_address;
+        return IterationDecision::Continue;
+    });
+}
