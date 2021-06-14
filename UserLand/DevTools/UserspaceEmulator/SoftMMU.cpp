ee192c6e82d806db8c6bd29aee686816ab7928f7
krishpranav <krisna.pranav@gmail.com>
2021-06-14 12:50:07 +0530

    [+] Added includes [+]

---
+++ UserLand/DevTools/UserspaceEmulator/SoftMMU.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include "SoftMMU.h"
+#include "Emulator.h"
+#include "MmapRegion.h"
+#include "Report.h"
+#include <AK/ByteBuffer.h>
+#include <AK/Memory.h>
+#include <AK/QuickSort.h>
