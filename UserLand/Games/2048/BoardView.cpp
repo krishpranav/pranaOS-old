93cc0c9a1ef5a9b291cfc01bec5522e30d368d50
krishpranav <krisna.pranav@gmail.com>
2021-06-14 17:49:16 +0530

    [+] set board [+]

--- UserLand/Games/2048/BoardView.cpp
+++ UserLand/Games/2048/BoardView.cpp
@@ -19,3 +19,23 @@ BoardView::BoardView(const Game::Board* board)
 BoardView::~BoardView()
 {
 }
+
+void BoardView::set_board(const Game::Board* board)
+{
+    if (m_board == board)
+        return;
+
+    if (!board) {
+        m_board = nullptr;
+        return;
+    }
+
+    bool must_resize = !m_board || m_board->size() != board->size();
+
+    m_board = board;
+
+    if (must_resize)
+        resize();
+
+    update();
+}
