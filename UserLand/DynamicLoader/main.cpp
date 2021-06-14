c0297198a568489a979d37176a70b0c40c137c4b
krishpranav <krisna.pranav@gmail.com>
2021-06-14 13:38:08 +0530

    [+] main cpp file updated [+]

--- UserLand/DynamicLoader/main.cpp
+++ UserLand/DynamicLoader/main.cpp
@@ -11,7 +11,7 @@
 #include <LibELF/AuxiliaryVector.h>
 #include <LibELF/DynamicLinker.h>
 
-char* __static_environ[] = { nullptr };
+char* __static_environ[] = { nullptr }; 
 
 static void init_libc()
 {
@@ -71,8 +71,49 @@ this helper program directly.
 
 extern "C" {
 
+
 void _start(int, char**, char**);
 
+void _start(int argc, char** argv, char** envp)
+{
+    char** env;
+    for (env = envp; *env; ++env) {
+    }
 
+    auxv_t* auxvp = (auxv_t*)++env;
+    perform_self_relocations(auxvp);
+    init_libc();
 
+    int main_program_fd = -1;
+    String main_program_name;
+    bool is_secure = false;
+    for (; auxvp->a_type != AT_NULL; ++auxvp) {
+        if (auxvp->a_type == ELF::AuxiliaryValue::ExecFileDescriptor) {
+            main_program_fd = auxvp->a_un.a_val;
+        }
+        if (auxvp->a_type == ELF::AuxiliaryValue::ExecFilename) {
+            main_program_name = (const char*)auxvp->a_un.a_ptr;
+        }
+        if (auxvp->a_type == ELF::AuxiliaryValue::Secure) {
+            is_secure = auxvp->a_un.a_val == 1;
+        }
+    }
+
+    if (main_program_name == "/usr/lib/Loader.so") {
+        display_help();
+        _exit(1);
+    }
+
+    VERIFY(main_program_fd >= 0);
+    VERIFY(!main_program_name.is_empty());
+
+    ELF::DynamicLinker::linker_main(move(main_program_name), main_program_fd, is_secure, argc, argv, envp);
+    VERIFY_NOT_REACHED();
+}
+
+void _fini();
+
+void _fini()
+{
+}
 }
