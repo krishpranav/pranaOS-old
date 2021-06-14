0381230ac99fe7ae36a2696c2ad19e2d05766e8b
krishpranav <krisna.pranav@gmail.com>
2021-06-14 17:47:29 +0530

    [+] includes [+]

---
+++ UserLand/Games/2048/BoardView.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include "BoardView.h"
+#include <LibGUI/Painter.h>
+#include <LibGfx/Font.h>
+#include <LibGfx/FontDatabase.h>
+#include <LibGfx/Palette.h>
