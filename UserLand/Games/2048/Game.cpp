59616d5565dafb67c930811c5d478e4299f6b748
krishpranav <krisna.pranav@gmail.com>
2021-06-15 08:52:50 +0530

    [+] grid size, ai [+]

--- UserLand/Games/2048/Game.cpp
+++ UserLand/Games/2048/Game.cpp
@@ -10,3 +10,10 @@
 #include <AK/NumericLimits.h>
 #include <AK/String.h>
 #include <stdlib.h>
+
+Game::Game(size_t grid_size, size_t target_tile, bool evil_ai)
+    : m_grid_size(grid_size)
+    , m_evil_ai(evil_ai)
+{
+    
+}
