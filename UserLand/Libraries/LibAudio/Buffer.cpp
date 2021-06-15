fd0f2c6482c0237b18284867d57e6c83672ef80c
krishpranav <krisna.pranav@gmail.com>
2021-06-15 16:25:30 +0530

    [+] buffer [+]

--- UserLand/Libraries/LibAudio/Buffer.cpp
+++ UserLand/Libraries/LibAudio/Buffer.cpp
@@ -81,5 +81,85 @@ static void read_samples_from_stream(InputMemoryStream& stream, SampleReader rea
     }
 }
 
+static double read_float_sample_64(InputMemoryStream& stream)
+{
+    LittleEndian<double> sample;
+    stream >> sample;
+    return double(sample);
+}
+
+static double read_float_sample_32(InputMemoryStream& stream)
+{
+    LittleEndian<float> sample;
+    stream >> sample;
+    return double(sample);
+}
+
+static double read_norm_sample_24(InputMemoryStream& stream)
+{
+    u8 byte = 0;
+    stream >> byte;
+    u32 sample1 = byte;
+    stream >> byte;
+    u32 sample2 = byte;
+    stream >> byte;
+    u32 sample3 = byte;
+
+    i32 value = 0;
+    value = sample1 << 8;
+    value |= (sample2 << 16);
+    value |= (sample3 << 24);
+    return double(value) / NumericLimits<i32>::max();
+}
+
+static double read_norm_sample_16(InputMemoryStream& stream)
+{
+    LittleEndian<i16> sample;
+    stream >> sample;
+    return double(sample) / NumericLimits<i16>::max();
+}
+
+static double read_norm_sample_8(InputMemoryStream& stream)
+{
+    u8 sample = 0;
+    stream >> sample;
+    return double(sample) / NumericLimits<u8>::max();
+}
+
+RefPtr<Buffer> Buffer::from_pcm_data(ReadonlyBytes data, ResampleHelper& resampler, int num_channels, PcmSampleFormat sample_format)
+{
+    InputMemoryStream stream { data };
+    return from_pcm_stream(stream, resampler, num_channels, sample_format, data.size() / (pcm_bits_per_sample(sample_format) / 8));
+}
+
+RefPtr<Buffer> Buffer::from_pcm_stream(InputMemoryStream& stream, ResampleHelper& resampler, int num_channels, PcmSampleFormat sample_format, int num_samples)
+{
+    Vector<Frame> fdata;
+    fdata.ensure_capacity(num_samples);
+
+    switch (sample_format) {
+    case PcmSampleFormat::Uint8:
+        read_samples_from_stream(stream, read_norm_sample_8, fdata, resampler, num_channels);
+        break;
+    case PcmSampleFormat::Int16:
+        read_samples_from_stream(stream, read_norm_sample_16, fdata, resampler, num_channels);
+        break;
+    case PcmSampleFormat::Int24:
+        read_samples_from_stream(stream, read_norm_sample_24, fdata, resampler, num_channels);
+        break;
+    case PcmSampleFormat::Float32:
+        read_samples_from_stream(stream, read_float_sample_32, fdata, resampler, num_channels);
+        break;
+    case PcmSampleFormat::Float64:
+        read_samples_from_stream(stream, read_float_sample_64, fdata, resampler, num_channels);
+        break;
+    default:
+        VERIFY_NOT_REACHED();
+    }
+
+    VERIFY(!stream.handle_any_error());
+
+    return Buffer::create_with_samples(move(fdata));
+}
 
 }
