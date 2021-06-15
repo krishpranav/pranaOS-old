75dd07fbd5496431ff86937a1b131fa84084779a
krishpranav <krisna.pranav@gmail.com>
2021-06-15 16:20:34 +0530

    [+] pcm bits per sample [+]

--- UserLand/Libraries/LibAudio/Buffer.cpp
+++ UserLand/Libraries/LibAudio/Buffer.cpp
@@ -9,3 +9,25 @@
 #include <AK/Debug.h>
 #include <AK/String.h>
 #include <LibAudio/Buffer.h>
+
+namespace Audio {
+
+u16 pcm_bits_per_sample(PcmSampleFormat format)
+{
+    switch (format) {
+    case Uint8:
+        return 8;
+    case Int16:
+        return 16;
+    case Int24:
+        return 24;
+    case Float32:
+        return 32;
+    case Float64:
+        return 64;
+    default:
+        VERIFY_NOT_REACHED();
+    }
+}
+
+}
