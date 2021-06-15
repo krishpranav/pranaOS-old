937e8845c9f337905ca1c39a9ab965920cef107c
krishpranav <krisna.pranav@gmail.com>
2021-06-15 16:11:58 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibAudio/Buffer.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <AK/ByteBuffer.h>
+#include <AK/MemoryStream.h>
+#include <AK/String.h>
+#include <AK/Types.h>
+#include <AK/Vector.h>
+#include <LibCore/AnonymousBuffer.h>
+#include <string.h>
