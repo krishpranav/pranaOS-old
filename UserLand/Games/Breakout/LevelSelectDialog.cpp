0ec683a02af9a8c8008c47c272f10acc0380b9bf
krishpranav <krisna.pranav@gmail.com>
2021-06-15 12:47:26 +0530

    [+] Level Select Dialog [+]

--- UserLand/Games/Breakout/LevelSelectDialog.cpp
+++ UserLand/Games/Breakout/LevelSelectDialog.cpp
@@ -11,3 +11,20 @@
 #include <LibGUI/Button.h>
 #include <LibGUI/Label.h>
 #include <LibGUI/ListView.h>
+
+namespace Breakout {
+
+
+LevelSelectDialog::LevelSelectDialog(Window* parent_window)
+    : Dialog(parent_window)
+{
+    set_rect(0, 0, 300, 250);
+    set_title("Level Select");
+    build();
+}
+
+LevelSelectDialog::~LevelSelectDialog()
+{
+}
+
+}
