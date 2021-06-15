c8035b0af2efa496fb36f838f66ce8b9538d2c4b
krishpranav <krisna.pranav@gmail.com>
2021-06-15 13:06:22 +0530

    [+] Tar file header expected checksum [+]

--- UserLand/Libraries/LibArchive/Tar.cpp
+++ UserLand/Libraries/LibArchive/Tar.cpp
@@ -6,3 +6,22 @@
 
 // includes
 #include "Tar.h"
+
+namespace Archive {
+
+unsigned TarFileHeader::expected_checksum() const
+{
+    auto checksum = 0u;
+    const u8* u8_this = reinterpret_cast<const u8*>(this);
+    const u8* u8_m_checksum = reinterpret_cast<const u8*>(&m_checksum);
+    for (auto i = 0u; i < sizeof(TarFileHeader); ++i) {
+        if (u8_this + i >= u8_m_checksum && u8_this + i < u8_m_checksum + sizeof(m_checksum)) {
+            checksum += ' ';
+        } else {
+            checksum += u8_this[i];
+        }
+    }
+    return checksum;
+}
+
+}
