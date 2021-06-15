c55e71beacf5ca0ff2f866eb3e8a426a293f7541
krishpranav <krisna.pranav@gmail.com>
2021-06-15 12:53:44 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibArchive/Tar.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+// includes
+#include <AK/String.h>
+#include <AK/StringView.h>
+#include <string.h>
+#include <sys/types.h>
