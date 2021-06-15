ec46db8642bd81805bc4482a0d3fd0dccbb2d278
krishpranav <krisna.pranav@gmail.com>
2021-06-15 14:54:24 +0530

    [+] read tar file stream [+]

--- UserLand/Libraries/LibArchive/TarStream.cpp
+++ UserLand/Libraries/LibArchive/TarStream.cpp
@@ -16,5 +16,19 @@ TarFileStream::TarFileStream(TarInputStream& tar_stream)
 {    
 }
 
+size_t TarFileStream::read(Bytes bytes)
+{
+    VERIFY(m_tar_stream.m_generation == m_generation);
+
+    if (has_any_error())
+        return 0;
+
+    auto to_read = min(bytes.size(), m_tar_stream.header().size() - m_tar_stream.m_file_offset);
+
+    auto nread = m_tar_stream.m_stream.read(bytes.trim(to_read));
+    m_tar_stream.m_file_offset += nread;
+    return nread;
+}
+
 
 }
