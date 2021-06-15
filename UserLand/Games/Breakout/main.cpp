e558b5cf823b5d8cc36945cc108f7ddfa564298a
krishpranav <krisna.pranav@gmail.com>
2021-06-15 12:48:48 +0530

    [+] includes [+]

---
+++ UserLand/Games/Breakout/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+// includes
+#include "Game.h"
+#include <LibGUI/Application.h>
+#include <LibGUI/Icon.h>
+#include <LibGUI/Menu.h>
+#include <LibGUI/Menubar.h>
+#include <LibGUI/Window.h>
+#include <LibGfx/Bitmap.h>
+#include <unistd.h>
