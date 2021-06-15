f3083971336e79fcd0eb8cf1fca45a1892537902
krishpranav <krisna.pranav@gmail.com>
2021-06-15 12:54:17 +0530

    [+] tar file type enum class [+]

--- UserLand/Libraries/LibArchive/Tar.h
+++ UserLand/Libraries/LibArchive/Tar.h
@@ -11,3 +11,22 @@
 #include <AK/StringView.h>
 #include <string.h>
 #include <sys/types.h>
+
+namespace Archive {
+
+enum class TarFileType : char {
+    NormalFile = '0',
+    AlternateNormalFile = '\0',
+    HardLink = '1',
+    SymLink = '2',
+    CharacterSpecialFile = '3',
+    BlockSpecialFile = '4',
+    Directory = '5',
+    FIFO = '6',
+    ContiguousFile = '7',
+    GlobalExtendedHeader = 'g',
+    ExtendedHeader = 'x'
+};
+
+
+}
