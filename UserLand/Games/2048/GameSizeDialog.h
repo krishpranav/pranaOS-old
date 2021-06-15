5865ffca91c4e2c5ad9456812af18477a32eb090
krishpranav <krisna.pranav@gmail.com>
2021-06-15 10:53:20 +0530

    [+] Added some public values [+]

--- UserLand/Games/2048/GameSizeDialog.h
+++ UserLand/Games/2048/GameSizeDialog.h
@@ -12,5 +12,9 @@
 
 class GameSizeDialog : public GUI::Dialog {
     C_OBJECT(GameSizeDialog)
-    
+public:
+    size_t board_size() const { return m_board_size; }
+    u32 target_tile() const { return 1u << m_target_tile_power; }
+    bool evil_ai() const { return m_evil_ai; }
+    bool temporary() const { return m_temporary; }
 }
