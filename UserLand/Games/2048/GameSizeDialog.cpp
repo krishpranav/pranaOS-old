a486402314a0165a392329ff7f3ed201bf2cad8b
krishpranav <krisna.pranav@gmail.com>
2021-06-15 10:55:51 +0530

    [+] includes [+]

---
+++ UserLand/Games/2048/GameSizeDialog.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+// includes
+#include "GameSizeDialog.h"
+#include "Game.h"
+#include <LibGUI/BoxLayout.h>
+#include <LibGUI/Button.h>
+#include <LibGUI/CheckBox.h>
+#include <LibGUI/Label.h>
+#include <LibGUI/SpinBox.h>
+#include <math.h>
