10b06108cab106bff946281239ffb5e0383390fa
krishpranav <krisna.pranav@gmail.com>
2021-06-15 14:59:34 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibArchive/Zip.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <AK/Function.h>
+#include <AK/IterationDecision.h>
+#include <AK/Span.h>
+#include <AK/Stream.h>
+#include <AK/String.h>
+#include <AK/Vector.h>
+#include <string.h>
