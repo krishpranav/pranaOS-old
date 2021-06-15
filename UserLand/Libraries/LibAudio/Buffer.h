69ecc97a7c8c6148ddb5af718784591878135fe0
krishpranav <krisna.pranav@gmail.com>
2021-06-15 16:13:12 +0530

    [+] frame struct [+]

--- UserLand/Libraries/LibAudio/Buffer.h
+++ UserLand/Libraries/LibAudio/Buffer.h
@@ -14,3 +14,58 @@
 #include <AK/Vector.h>
 #include <LibCore/AnonymousBuffer.h>
 #include <string.h>
+
+namespace Audio {
+
+
+struct Frame {
+    Frame()
+        : left(0)
+        , right(0)
+    {
+    }
+
+    Frame(double left)
+        : left(left)
+        , right(left)
+    {
+    }
+
+    Frame(double left, double right)
+        : left(left)
+        , right(right)
+    {
+    }
+
+    void clip()
+    {
+        if (left > 1)
+            left = 1;
+        else if (left < -1)
+            left = -1;
+
+        if (right > 1)
+            right = 1;
+        else if (right < -1)
+            right = -1;
+    }
+
+    void scale(int percent)
+    {
+        double pct = (double)percent / 100.0;
+        left *= pct;
+        right *= pct;
+    }
+
+    Frame& operator+=(const Frame& other)
+    {
+        left += other.left;
+        right += other.right;
+        return *this;
+    }
+
+    double left;
+    double right;
+};
+
+}
