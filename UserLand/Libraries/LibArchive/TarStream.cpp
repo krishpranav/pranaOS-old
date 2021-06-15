5be694cf2f55149c37c326743159212ee7fbbb32
krishpranav <krisna.pranav@gmail.com>
2021-06-15 14:51:04 +0530

    [+] Tar input Stream [+]

--- UserLand/Libraries/LibArchive/TarStream.cpp
+++ UserLand/Libraries/LibArchive/TarStream.cpp
@@ -8,3 +8,13 @@
 // includes
 #include <LibArchive/TarStream.h>
 #include <string.h>
+
+namespace Archive {
+TarFileStream::TarFileStream(TarInputStream& tar_stream)
+    : m_tar_stream(tar_stream)
+    , m_generation(tar_stream.m_generation)
+{    
+}
+
+
+}
