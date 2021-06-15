7c252c91a1ae25608e0ed31dc2a3d23f469ec20a
krishpranav <krisna.pranav@gmail.com>
2021-06-15 13:12:27 +0530

    [+] Tar stream header [+]

--- UserLand/Libraries/LibArchive/TarStream.h
+++ UserLand/Libraries/LibArchive/TarStream.h
@@ -31,4 +31,37 @@ private:
     friend class TarInputStream;
 };
 
+class TarInputStream {
+public:
+    TarInputStream(InputStream&);
+    void advance();
+    bool finished() const { return m_finished; }
+    bool valid() const;
+    const TarFileHeader& header() const { return m_header; }
+    TarFileStream file_contents();
+
+private:
+    TarFileHeader m_header;
+    InputStream& m_stream;
+    unsigned long m_file_offset { 0 };
+    int m_generation { 0 };
+    bool m_finished { false };
+
+    friend class TarFileStream;
+};
+
+class TarOutputStream {
+public:
+    TarOutputStream(OutputStream&);
+    void add_file(const String& path, mode_t, const ReadonlyBytes&);
+    void add_directory(const String& path, mode_t);
+    void finish();
+
+private:
+    OutputStream& m_stream;
+    bool m_finished { false };
+
+    friend class TarFileStream;
+};
+
 }
