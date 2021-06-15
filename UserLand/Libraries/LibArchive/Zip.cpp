4ef3850840eda5082af719306eaa0f93d2ed8617
krishpranav <krisna.pranav@gmail.com>
2021-06-15 15:09:02 +0530

    [+] try create zip [+]

--- UserLand/Libraries/LibArchive/Zip.cpp
+++ UserLand/Libraries/LibArchive/Zip.cpp
@@ -25,4 +25,54 @@ bool Zip::find_end_of_central_directory_offset(const ReadonlyBytes& buffer, size
     return false;
 }
 
+Optional<Zip> Zip::try_create(const ReadonlyBytes& buffer)
+{
+    size_t end_of_central_directory_offset;
+    if (!find_end_of_central_directory_offset(buffer, end_of_central_directory_offset))
+        return {};
+
+    EndOfCentralDirectory end_of_central_directory {};
+    if (!end_of_central_directory.read(buffer.slice(end_of_central_directory_offset)))
+        return {};
+
+    if (end_of_central_directory.disk_number != 0 || end_of_central_directory.central_directory_start_disk != 0 || end_of_central_directory.disk_records_count != end_of_central_directory.total_records_count)
+        return {}; 
+
+    size_t member_offset = end_of_central_directory.central_directory_offset;
+    for (size_t i = 0; i < end_of_central_directory.total_records_count; i++) {
+        CentralDirectoryRecord central_directory_record {};
+        if (member_offset > buffer.size())
+            return {};
+        if (!central_directory_record.read(buffer.slice(member_offset)))
+            return {};
+        if (central_directory_record.general_purpose_flags & 1)
+            return {}; 
+        if (central_directory_record.general_purpose_flags & 3)
+            return {}; 
+        if (central_directory_record.compression_method != ZipCompressionMethod::Store && central_directory_record.compression_method != ZipCompressionMethod::Deflate)
+            return {}; 
+        if (central_directory_record.compression_method == ZipCompressionMethod::Store && central_directory_record.uncompressed_size != central_directory_record.compressed_size)
+            return {};
+        if (central_directory_record.start_disk != 0)
+            return {};
+        if (memchr(central_directory_record.name, 0, central_directory_record.name_length) != nullptr)
+            return {};
+        LocalFileHeader local_file_header {};
+        if (central_directory_record.local_file_header_offset > buffer.size())
+            return {};
+        if (!local_file_header.read(buffer.slice(central_directory_record.local_file_header_offset)))
+            return {};
+        if (buffer.size() - (local_file_header.compressed_data - buffer.data()) < central_directory_record.compressed_size)
+            return {};
+        member_offset += central_directory_record.size();
+    }
+
+    Zip zip;
+    zip.m_input_data = buffer;
+    zip.member_count = end_of_central_directory.total_records_count;
+    zip.members_start_offset = end_of_central_directory.central_directory_offset;
+    return zip;
+}
+
+
 }
