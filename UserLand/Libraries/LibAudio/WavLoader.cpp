f1e2ace066fc7883bc94ffb787393e5d3b67e591
krishpranav <krisna.pranav@gmail.com>
2021-06-16 10:16:35 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibAudio/WavLoader.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+
+// includes
+#include <AK/Debug.h>
+#include <AK/NumericLimits.h>
+#include <AK/OwnPtr.h>
+#include <LibAudio/Buffer.h>
+#include <LibAudio/WavLoader.h>
+#include <LibCore/File.h>
+#include <LibCore/FileStream.h>
