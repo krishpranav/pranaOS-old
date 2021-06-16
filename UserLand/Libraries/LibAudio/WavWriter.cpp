1eeda7a6c65d7b88a26ffe2a6489b324e8a49845
krishpranav <krisna.pranav@gmail.com>
2021-06-16 11:14:36 +0530

    [+] Wav writter [+]

--- UserLand/Libraries/LibAudio/WavWriter.cpp
+++ UserLand/Libraries/LibAudio/WavWriter.cpp
@@ -6,3 +6,15 @@
 
 // includes
 #include <LibAudio/WavWriter.h>
+
+namespace Audio {
+
+WavWriter::WavWriter(const StringView& path, int sample_rate, int num_channels, int bits_per_sample)
+    : m_sample_rate(sample_rate)
+    , m_num_channels(num_channels)
+    , m_bits_per_sample(bits_per_sample)
+{
+    set_file(path);
+}
+
+}
