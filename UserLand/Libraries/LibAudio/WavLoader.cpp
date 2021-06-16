7815aa5cac43fa1b44089f69c55ab7b42125c3ce
krishpranav <krisna.pranav@gmail.com>
2021-06-16 10:45:09 +0530

    [+] update [+]

--- UserLand/Libraries/LibAudio/WavLoader.cpp
+++ UserLand/Libraries/LibAudio/WavLoader.cpp
@@ -50,4 +50,39 @@ WavLoaderPlugin::WavLoaderPlugin(const ByteBuffer& buffer)
     m_resampler = make<ResampleHelper>(m_sample_rate, 44100);
 }
 
+RefPtr<Buffer> WavLoaderPlugin::get_more_samples(size_t max_bytes_to_read_from_input)
+{
+    if (!m_stream)
+        return nullptr;
+
+    size_t bytes_per_sample = (m_num_channels * (pcm_bits_per_sample(m_sample_format) / 8));
+
+    // Might truncate if not evenly divisible
+    size_t samples_to_read = static_cast<int>(max_bytes_to_read_from_input) / bytes_per_sample;
+    size_t bytes_to_read = samples_to_read * bytes_per_sample;
+
+    dbgln_if(AWAVLOADER_DEBUG, "Read {} bytes ({} samples) WAV with num_channels {} sample rate {}, "
+                               "bits per sample {}, sample format {}",
+        bytes_to_read, samples_to_read, m_num_channels, m_sample_rate,
+        pcm_bits_per_sample(m_sample_format), sample_format_name(m_sample_format));
+
+    ByteBuffer sample_data = ByteBuffer::create_zeroed(bytes_to_read);
+    m_stream->read_or_error(sample_data.bytes());
+    if (m_stream->handle_any_error()) {
+        return nullptr;
+    }
+
+    RefPtr<Buffer> buffer = Buffer::from_pcm_data(
+        sample_data.bytes(),
+        *m_resampler,
+        m_num_channels,
+        m_sample_format);
+
+    // m_loaded_samples should contain the amount of actually loaded samples
+    m_loaded_samples += samples_to_read;
+    m_loaded_samples = min(m_total_samples, m_loaded_samples);
+    return buffer;
+}
+
+
 }
