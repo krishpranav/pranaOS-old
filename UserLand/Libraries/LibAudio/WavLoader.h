7d6671469d74865a17fae4718160b236bf523247
krishpranav <krisna.pranav@gmail.com>
2021-06-16 10:15:28 +0530

    [+] public functions [+]

--- UserLand/Libraries/LibAudio/WavLoader.h
+++ UserLand/Libraries/LibAudio/WavLoader.h
@@ -30,4 +30,31 @@ class Buffer;
 #define WAVE_FORMAT_EXTENSIBLE 0xFFFE // Determined by SubFormat
 
 
+class WavLoaderPlugin : public LoaderPlugin {
+public:
+    WavLoaderPlugin(const StringView& path);
+    WavLoaderPlugin(const ByteBuffer& buffer);
+
+    virtual bool sniff() override { return valid; }
+
+    virtual bool has_error() override { return !m_error_string.is_null(); }
+    virtual const char* error_string() override { return m_error_string.characters(); }
+
+    virtual RefPtr<Buffer> get_more_samples(size_t max_bytes_to_read_from_input = 128 * KiB) override;
+
+    virtual void reset() override { return seek(0); }
+
+    // sample_index 0 is the start of the raw audio sample data
+    // within the file/stream.
+    virtual void seek(const int sample_index) override;
+
+    virtual int loaded_samples() override { return m_loaded_samples; }
+    virtual int total_samples() override { return m_total_samples; }
+    virtual u32 sample_rate() override { return m_sample_rate; }
+    virtual u16 num_channels() override { return m_num_channels; }
+    virtual PcmSampleFormat pcm_format() override { return m_sample_format; }
+    virtual RefPtr<Core::File> file() override { return m_file; }
+
+}
+
 }
