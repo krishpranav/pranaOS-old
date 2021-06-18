f844e58a991d45400233773fb07de9224a97e0c2
krishpranav <krisna.pranav@gmail.com>
2021-06-18 12:47:01 +0530

    [+] DEBUG [+]

--- UserLand/Libraries/LibC/assert.h
+++ UserLand/Libraries/LibC/assert.h
@@ -10,3 +10,18 @@
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
+
+#ifdef DEBUG
+[[noreturn]] void __assertion_failed(const char* msg);
+#    define __stringify_helper(x) #    x
+#    define __stringify(x) __stringify_helper(x)
+#    define assert(expr)                                                           \
+        do {                                                                       \
+            if (__builtin_expect(!(expr), 0))                                      \
+                __assertion_failed(#expr "\n" __FILE__ ":" __stringify(__LINE__)); \
+        } while (0)
+#else
+#    define assert(expr) ((void)(0))
+#    define VERIFY_NOT_REACHED() _abort()
+#endif
+
