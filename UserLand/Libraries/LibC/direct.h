015e60e3ea5c92ffc9a9796215a43dc76a6b02ef
krishpranav <krisna.pranav@gmail.com>
2021-06-18 15:53:41 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/direct.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once 
+
+// includes
+#include <sys/cdefs.h>
+#include <sys/types.h>
