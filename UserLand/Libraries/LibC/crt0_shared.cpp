4bd42a51011863e4ae597bf767dcb41cb702c5cc
krishpranav <krisna.pranav@gmail.com>
2021-06-18 12:55:45 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/crt0_shared.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <AK/Types.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/internals.h>
+#include <unistd.h>
