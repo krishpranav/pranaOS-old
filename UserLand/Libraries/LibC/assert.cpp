d330c88769c3296c31e931cd07496b71bce74c9f
krishpranav <krisna.pranav@gmail.com>
2021-06-18 12:50:24 +0530

    [+] assertion failed functions [+]

--- UserLand/Libraries/LibC/assert.cpp
+++ UserLand/Libraries/LibC/assert.cpp
@@ -18,10 +18,19 @@ extern "C" {
 
 
 extern bool __stdio_is_initialized;
-#ifndef DEBUG 
+#ifdef DEBUG
 void __assertion_failed(const char* msg)
 {
-    
-}
+    dbgln("USERSPACE({}) ASSERTION FAILED: {}", getpid(), msg);
+    if (__stdio_is_initialized)
+        warnln("ASSERTION FAILED: {}", msg);
 
+    Syscall::SC_set_coredump_metadata_params params {
+        { "assertion", strlen("assertion") },
+        { msg, strlen(msg) },
+    };
+    syscall(SC_set_coredump_metadata, &params);
+    abort();
+}
+#endif
 }
