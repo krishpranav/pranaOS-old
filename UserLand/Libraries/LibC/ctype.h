d7adc28d4b2660d013d0735cc8420ffada924471
krishpranav <krisna.pranav@gmail.com>
2021-06-18 15:22:46 +0530

    [+] is int values [+]

--- UserLand/Libraries/LibC/ctype.h
+++ UserLand/Libraries/LibC/ctype.h
@@ -105,3 +105,26 @@ static inline int __inline_toupper(int c)
         return c & ~0x20;
     return c;
 }
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int isalnum(int c);
+int isalpha(int c);
+int isascii(int c);
+int iscntrl(int c);
+int isdigit(int c);
+int isxdigit(int c);
+int isspace(int c);
+int ispunct(int c);
+int isprint(int c);
+int isgraph(int c);
+int islower(int c);
+int isupper(int c);
+int isblank(int c);
+int toascii(int c);
+int tolower(int c);
+int toupper(int c);
+
+}
