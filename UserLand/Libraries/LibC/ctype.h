908d37326e79526d19bfdff777dba5cb0f98d1bd
krishpranav <krisna.pranav@gmail.com>
2021-06-18 15:22:18 +0530

    [+] inline values [+]

--- UserLand/Libraries/LibC/ctype.h
+++ UserLand/Libraries/LibC/ctype.h
@@ -21,3 +21,87 @@ __BEGIN_DECLS
 #define _B 0200
 
 extern const char _ctype_[256];
+
+static inline int __inline_isalnum(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_U | _L | _N);
+}
+
+static inline int __inline_isalpha(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_U | _L);
+}
+
+static inline int __inline_isascii(int c)
+{
+    return (unsigned)c <= 127;
+}
+
+static inline int __inline_iscntrl(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_C);
+}
+
+static inline int __inline_isdigit(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_N);
+}
+
+static inline int __inline_isxdigit(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_N | _X);
+}
+
+static inline int __inline_isspace(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_S);
+}
+
+static inline int __inline_ispunct(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_P);
+}
+
+static inline int __inline_isprint(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_P | _U | _L | _N | _B);
+}
+
+static inline int __inline_isgraph(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_P | _U | _L | _N);
+}
+
+static inline int __inline_islower(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_L);
+}
+
+static inline int __inline_isupper(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_U);
+}
+
+static inline int __inline_isblank(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_B) || (c == '\t');
+}
+
+static inline int __inline_toascii(int c)
+{
+    return c & 127;
+}
+
+static inline int __inline_tolower(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return c | 0x20;
+    return c;
+}
+
+static inline int __inline_toupper(int c)
+{
+    if (c >= 'a' && c <= 'z')
+        return c & ~0x20;
+    return c;
+}
