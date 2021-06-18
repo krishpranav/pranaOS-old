fbcfd04782789e54a033080c934477a5ff57aec5
krishpranav <krisna.pranav@gmail.com>
2021-06-18 15:20:50 +0530

    [+] ctypes [+]

--- UserLand/Libraries/LibC/ctype.cpp
+++ UserLand/Libraries/LibC/ctype.cpp
@@ -40,4 +40,87 @@ int isalpha(int c)
     return __inline_isalpha(c);
 }
 
+#undef iscntrl
+int iscntrl(int c)
+{
+    return __inline_iscntrl(c);
+}
+
+#undef isdigit
+int isdigit(int c)
+{
+    return __inline_isdigit(c);
+}
+
+#undef isxdigit
+int isxdigit(int c)
+{
+    return __inline_isxdigit(c);
+}
+
+#undef isspace
+int isspace(int c)
+{
+    return __inline_isspace(c);
+}
+
+#undef ispunct
+int ispunct(int c)
+{
+    return __inline_ispunct(c);
+}
+
+#undef isprint
+int isprint(int c)
+{
+    return __inline_isprint(c);
+}
+
+#undef isgraph
+int isgraph(int c)
+{
+    return __inline_isgraph(c);
+}
+
+#undef isupper
+int isupper(int c)
+{
+    return __inline_isupper(c);
+}
+
+#undef islower
+int islower(int c)
+{
+    return __inline_islower(c);
+}
+
+#undef isascii
+int isascii(int c)
+{
+    return __inline_isascii(c);
+}
+
+#undef isblank
+int isblank(int c)
+{
+    return __inline_isblank(c);
+}
+
+#undef toascii
+int toascii(int c)
+{
+    return __inline_toascii(c);
+}
+
+#undef tolower
+int tolower(int c)
+{
+    return __inline_tolower(c);
+}
+
+#undef toupper
+int toupper(int c)
+{
+    return __inline_toupper(c);
+}
 }
