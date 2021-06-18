4792fea104314eb805988ded749963d32821429d
krishpranav <krisna.pranav@gmail.com>
2021-06-18 12:58:18 +0530

    [+] start function for crt0 [+]

--- UserLand/Libraries/LibC/crt0.cpp
+++ UserLand/Libraries/LibC/crt0.cpp
@@ -18,4 +18,31 @@ extern "C" {
 extern u32 __stack_chk_guard;
 
 int main(int, char**, char**);
+
+
+int _start(int argc, char** argv, char** env);
+
+int _start(int argc, char** argv, char** env)
+{
+    u32 original_stack_chk = __stack_chk_guard;
+    arc4random_buf(&__stack_chk_guard, sizeof(__stack_chk_guard));
+
+    if (__stack_chk_guard == 0)
+        __stack_chk_guard = original_stack_chk;
+
+    environ = env;
+    __environ_is_malloced = false;
+
+    _init();
+
+    int status = main(argc, argv, environ);
+
+    exit(status);
+
+
+    __stack_chk_guard = original_stack_chk;
+
+    return 20150614;
 }
+}
+#endif
