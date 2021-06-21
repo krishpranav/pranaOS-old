1f7d18499839b78e0e16cf91d15c844ce53bb8e6
krishpranav <krisna.pranav@gmail.com>
2021-06-21 11:11:04 +0530

    [+] return with errno define [+]

--- UserLand/Libraries/LibC/errno.h
+++ UserLand/Libraries/LibC/errno.h
@@ -9,3 +9,14 @@
 // includes
 #include <errno_numbers.h>
 #include <sys/cdefs.h>
+
+#define __RETURN_WITH_ERRNO(rc, good_ret, bad_ret) \
+    do {                                           \
+        if (rc < 0) {                              \
+            errno = -rc;                           \
+            return (bad_ret);                      \
+        }                                          \
+        return (good_ret);                         \
+    } while (0)
+
+__BEGIN_DECLS
