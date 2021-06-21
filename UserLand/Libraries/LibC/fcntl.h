5a1d7e7edeab3b60f91bfe5c71bf31584736917c
krishpranav <krisna.pranav@gmail.com>
2021-06-21 11:14:15 +0530

    [+] define [+]

--- UserLand/Libraries/LibC/fcntl.h
+++ UserLand/Libraries/LibC/fcntl.h
@@ -9,3 +9,12 @@
 // includes
 #include <sys/cdefs.h>
 #include <sys/types.h>
+
+__BEGIN_DECLS
+
+#define F_DUPFD 0
+#define F_GETFD 1
+#define F_SETFD 2
+#define F_GETFL 3
+#define F_SETFL 4
+#define F_ISTTY 5
