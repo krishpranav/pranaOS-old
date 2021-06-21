8c801c80e2facdd5734ec715cecb21e851d7cfce
krishpranav <krisna.pranav@gmail.com>
2021-06-21 08:36:22 +0530

    [+] elf typedef [+]

--- UserLand/Libraries/LibC/elf.h
+++ UserLand/Libraries/LibC/elf.h
@@ -12,3 +12,31 @@
 #else
 #    include <AK/Types.h>
 #endif
+
+typedef uint8_t Elf_Byte;
+
+typedef uint32_t Elf32_Addr;
+typedef uint32_t Elf32_Off; 
+typedef int32_t Elf32_Sword;
+typedef uint32_t Elf32_Word;
+typedef uint16_t Elf32_Half;
+typedef uint64_t Elf32_Lword;
+
+typedef uint64_t Elf64_Addr;
+typedef uint64_t Elf64_Off;
+typedef int32_t Elf64_Shalf;
+
+#ifdef __alpha__
+typedef int64_t Elf64_Sword;
+typedef uint64_t Elf64_Word;
+#else
+typedef int32_t Elf64_Sword;
+typedef uint32_t Elf64_Word;
+#endif
+
+typedef int64_t Elf64_Sxword;
+typedef uint64_t Elf64_Xword;
+typedef uint64_t Elf64_Lword;
+
+typedef uint32_t Elf64_Half;
+typedef uint16_t Elf64_Quarter;
