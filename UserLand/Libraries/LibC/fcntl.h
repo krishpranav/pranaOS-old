fa279dae0925cce6e3a1a66bcf8db1f68781de4e
krishpranav <krisna.pranav@gmail.com>
2021-06-21 11:14:42 +0530

    [+] defin evalues [+]

--- UserLand/Libraries/LibC/fcntl.h
+++ UserLand/Libraries/LibC/fcntl.h
@@ -21,3 +21,19 @@ __BEGIN_DECLS
 
 
 #define FD_CLOEXEC 1
+
+#define O_RDONLY (1 << 0)
+#define O_WRONLY (1 << 1)
+#define O_RDWR (O_RDONLY | O_WRONLY)
+#define O_ACCMODE (O_RDONLY | O_WRONLY)
+#define O_EXEC (1 << 2)
+#define O_CREAT (1 << 3)
+#define O_EXCL (1 << 4)
+#define O_NOCTTY (1 << 5)
+#define O_TRUNC (1 << 6)
+#define O_APPEND (1 << 7)
+#define O_NONBLOCK (1 << 8)
+#define O_DIRECTORY (1 << 9)
+#define O_NOFOLLOW (1 << 10)
+#define O_CLOEXEC (1 << 11)
+#define O_DIRECT (1 << 12)
