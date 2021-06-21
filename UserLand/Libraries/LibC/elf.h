46b7f9cfee4090ced03194e5c1aae632d0d58bb5
krishpranav <krisna.pranav@gmail.com>
2021-06-21 10:33:26 +0530

    [+] file id [+]

--- UserLand/Libraries/LibC/elf.h
+++ UserLand/Libraries/LibC/elf.h
@@ -40,3 +40,15 @@ typedef uint64_t Elf64_Lword;
 
 typedef uint32_t Elf64_Half;
 typedef uint16_t Elf64_Quarter;
+
+#define EI_MAG0 0       /* file ID */
+#define EI_MAG1 1       /* file ID */
+#define EI_MAG2 2       /* file ID */
+#define EI_MAG3 3       /* file ID */
+#define EI_CLASS 4      /* file class */
+#define EI_DATA 5       /* data encoding */
+#define EI_VERSION 6    /* ELF header version */
+#define EI_OSABI 7      /* OS/ABI ID */
+#define EI_ABIVERSION 8 /* ABI version */
+#define EI_PAD 9        /* start of pad bytes */
+#define EI_NIDENT 16    /* Gfx::Size of e_ident[] */
