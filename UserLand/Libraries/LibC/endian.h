7be94aa05857fe2c67852a188a277c9a84151d19
krishpranav <krisna.pranav@gmail.com>
2021-06-21 11:06:18 +0530

    [+] gnuc [+]

--- UserLand/Libraries/LibC/endian.h
+++ UserLand/Libraries/LibC/endian.h
@@ -13,3 +13,9 @@ __BEGIN_DECLS
 #define __LITTLE_ENDIAN 1234
 #define __BIG_ENDIAN 4321
 #define __PDP_ENDIAN 3412
+
+#if defined(__GNUC__) && defined(__BYTE_ORDER__)
+#    define __BYTE_ORDER __BYTE_ORDER__
+#else
+#    include <bits/endian.h>
+#endif
