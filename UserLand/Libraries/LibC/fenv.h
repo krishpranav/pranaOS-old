cd6b445b5b918287947c6ffae11be7af63dd6d63
krishpranav <krisna.pranav@gmail.com>
2021-06-21 13:03:26 +0530

    [+] x87 floating point env [+]

--- UserLand/Libraries/LibC/fenv.h
+++ UserLand/Libraries/LibC/fenv.h
@@ -9,3 +9,20 @@
 // incldes
 #include <stdint.h>
 #include <sys/cdefs.h>
+
+
+struct __x87_floating_point_environment {
+    uint16_t __control_word;
+    uint16_t __reserved1;
+    uint16_t __status_word;
+    uint16_t __reserved2;
+    uint16_t __tag_word;
+    uint16_t __reserved3;
+    uint32_t __fpu_ip_offset;
+    uint16_t __fpu_ip_selector;
+    uint16_t __opcode : 11;
+    uint16_t __reserved4 : 5;
+    uint32_t __fpu_data_offset;
+    uint16_t __fpu_data_selector;
+    uint16_t __reserved5;
+};
