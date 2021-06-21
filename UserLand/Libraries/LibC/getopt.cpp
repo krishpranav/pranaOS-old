52fb9a1c336036691676c0791c05cb21d35292d6
krishpranav <krisna.pranav@gmail.com>
2021-06-21 13:50:19 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/getopt.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <AK/StringView.h>
+#include <AK/Vector.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
