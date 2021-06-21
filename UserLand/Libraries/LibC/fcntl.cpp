a9ffbe7e1bbdfbc22a43591606df5fae0ba08c6f
krishpranav <krisna.pranav@gmail.com>
2021-06-21 11:23:12 +0530

    [+] fcntl [+]

--- UserLand/Libraries/LibC/fcntl.cpp
+++ UserLand/Libraries/LibC/fcntl.cpp
@@ -10,3 +10,17 @@
 #include <stdarg.h>
 #include <string.h>
 #include <syscall.h>
+
+extern "C" {
+
+int fcntl(int fd, int cmd, ...)
+{
+    va_list ap;
+    va_start(ap, cmd);
+    u32 extra_arg = va_arg(ap, u32);
+    int rc = syscall(SC_fcntl, fd, cmd, extra_arg);
+    va_end(ap);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+}
