f76425d1f19dc913113d52faf064a483cb37f8b3
krishpranav <krisna.pranav@gmail.com>
2021-06-21 13:48:35 +0530

    [+] option structure [+]

--- UserLand/Libraries/LibC/getopt.h
+++ UserLand/Libraries/LibC/getopt.h
@@ -14,3 +14,10 @@ __BEGIN_DECLS
 #define no_argument 0
 #define required_argument 1
 #define optional_argument 2
+
+struct option {
+    const char* name;
+    int has_arg;
+    int* flag;
+    int val;
+};
