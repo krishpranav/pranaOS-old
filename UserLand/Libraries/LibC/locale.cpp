7c7684831d7cd0f4575fa192520d3eecc01af18f
krishpranav <krisna.pranav@gmail.com>
2021-06-22 11:26:05 +0530

    [+] lconv [+]

--- UserLand/Libraries/LibC/locale.cpp
+++ UserLand/Libraries/LibC/locale.cpp
@@ -19,5 +19,31 @@ static char default_grouping[] = "\x03\x03";
 static char default_empty_string[] = "";
 static char default_empty_value = 127;
 
+static struct lconv default_locale = {
+    default_decimal_point,
+    default_thousands_sep,
+    default_grouping,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value
+};
 
 }
