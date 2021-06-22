98aa7da2ac2d07ea6d61ee9306f65e25916741a7
krishpranav <krisna.pranav@gmail.com>
2021-06-22 11:25:29 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/locale.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <assert.h>
+#include <locale.h>
+#include <stdio.h>
+#include <string.h>
