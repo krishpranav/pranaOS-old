f082bc995a64d170534824979b997103495ab460
krishpranav <krisna.pranav@gmail.com>
2021-06-22 12:36:09 +0530

    [+] hostent [+]

--- UserLand/Libraries/LibC/netdb.h
+++ UserLand/Libraries/LibC/netdb.h
@@ -9,3 +9,14 @@
 // includes
 #include <sys/cdefs.h>
 #include <sys/types.h>
+
+__BEGIN_DECLS
+
+struct hostent {
+    char* h_name;
+    char** h_aliases;
+    int h_addrtype;
+    int h_length;
+    char** h_addr_list;
+#define h_addr h_addr_list[0]
+};
