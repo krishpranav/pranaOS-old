125297626d105f8d88f8152685848e5b63760ba8
krishpranav <krisna.pranav@gmail.com>
2021-06-22 10:47:35 +0530

    [+] dirname [+]

--- UserLand/Libraries/LibC/libgen.cpp
+++ UserLand/Libraries/LibC/libgen.cpp
@@ -11,3 +11,28 @@
 
 static char dot[] = ".";
 static char slash[] = "/";
+
+char* dirname(char* path)
+{
+    if (path == nullptr)
+        return dot;
+
+    int len = strlen(path);
+    if (len == 0)
+        return dot;
+
+    while (len > 1 && path[len - 1] == '/') {
+        path[len - 1] = 0;
+        len--;
+    }
+
+    char* last_slash = strrchr(path, '/');
+    if (last_slash == nullptr)
+        return dot;
+
+    if (last_slash == path)
+        return slash;
+
+    *last_slash = 0;
+    return path;
+}
