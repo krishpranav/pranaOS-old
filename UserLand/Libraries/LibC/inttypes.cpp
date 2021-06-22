879854bd6a56a01040e41ac3dde555863775eab4
krishpranav <krisna.pranav@gmail.com>
2021-06-22 10:30:42 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/inttypes.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <AK/NumericLimits.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <stdlib.h>
