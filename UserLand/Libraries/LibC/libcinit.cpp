2af06397ae5a86bc9b6a578006e393ce1fea91f9
krishpranav <krisna.pranav@gmail.com>
2021-06-22 10:42:56 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/libcinit.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <AK/Types.h>
+#include <assert.h>
+#include <sys/internals.h>
+#include <unistd.h>
