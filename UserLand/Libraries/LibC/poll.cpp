6b9ca33fc0381f620a9aeff882cccc0b6d242558
krishpranav <krisna.pranav@gmail.com>
2021-06-22 18:45:19 +0530

    [+] poll functions [+]

--- UserLand/Libraries/LibC/poll.cpp
+++ UserLand/Libraries/LibC/poll.cpp
@@ -9,3 +9,18 @@
 #include <poll.h>
 #include <sys/time.h>
 #include <syscall.h>
+
+extern "C" {
+
+  int poll(pollfd* fds, nfds_t nfds, int timeout_ms)
+  {
+      timespec timeout;
+      timespec* timeout_ts = &timeout;
+      if (timeout_ms < 0)
+          timeout_ts = nullptr;
+      else
+          timeout = { timeout_ms / 1000, (timeout_ms % 1000) * 1'000'000 };
+      return ppoll(fds, nfds, timeout_ts, nullptr);
+  }
+
+}
