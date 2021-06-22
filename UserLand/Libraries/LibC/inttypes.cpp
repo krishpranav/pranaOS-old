4b0f6ab204c4a681a8a58619cfca3c7db62fa9c8
krishpranav <krisna.pranav@gmail.com>
2021-06-22 10:32:04 +0530

    [+] imaxdiv [+]

--- UserLand/Libraries/LibC/inttypes.cpp
+++ UserLand/Libraries/LibC/inttypes.cpp
@@ -9,3 +9,22 @@
 #include <errno.h>
 #include <inttypes.h>
 #include <stdlib.h>
+
+extern "C" {
+
+imaxdiv_t imaxdiv(intmax_t numerator, intmax_t denominator)
+{
+    imaxdiv_t result;
+    result.quot = numerator / denominator;
+    result.rem = numerator % denominator;
+
+    if (numerator >= 0 && result.rem < 0) {
+        result.quot++;
+        result.rem -= denominator;
+    }
+
+    return result;
+
+}
+
+}
