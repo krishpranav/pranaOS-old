1aa8703732487593d1b8250ebcda900229d45b05
krishpranav <krisna.pranav@gmail.com>
2021-06-22 11:19:19 +0530

    [+] ifndef [+]

---
+++ UserLand/Libraries/LibC/link.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#ifndef __pranaos__
+#    include <LibC/elf.h>
+#else
+#    include <elf.h>
+#endif
+#include <limits.h>
+#include <sys/cdefs.h>
