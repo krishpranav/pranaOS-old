41a9bb22cb7562e05a2a5e4bfc22bc1f9a11badc
krishpranav <krisna.pranav@gmail.com>
2021-06-22 12:33:41 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/net.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <LibC/errno.h>
+#include <LibC/net/if.h>
+#include <LibC/netinet/in.h>
