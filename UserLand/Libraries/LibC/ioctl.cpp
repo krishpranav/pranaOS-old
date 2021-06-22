b77801519399484ec8a1ff2ba6ff459b7f3ef3d5
krishpranav <krisna.pranav@gmail.com>
2021-06-22 10:39:59 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/ioctl.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <syscall.h>
