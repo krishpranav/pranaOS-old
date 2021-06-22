07bf1da4f6451e8eba5a01713cc3fb87f0c60f74
krishpranav <krisna.pranav@gmail.com>
2021-06-22 12:30:28 +0530

    [+] mntent structure [+]

--- UserLand/Libraries/LibC/mmtent.h
+++ UserLand/Libraries/LibC/mmtent.h
@@ -15,5 +15,13 @@ __BEGIN_DECLS
 #define MOUNTED "/etc/mtab"
 #define MNTTAB "/etc/fstab"
 
+struct mntent {
+    char* mnt_fsname;
+    char* mnt_dir;
+    char* mnt_type;
+    char* mnt_opts;
+    int mnt_freq;
+    int mnt_passno;
+};
 
 struct mntent* getmntent(FILE* stream);
