4c3d2445dee2d0f0e595e0abf2bf5c615d5ee33d
krishpranav <krisna.pranav@gmail.com>
2021-06-22 11:25:50 +0530

    [+] static char [+]

--- UserLand/Libraries/LibC/locale.cpp
+++ UserLand/Libraries/LibC/locale.cpp
@@ -9,3 +9,15 @@
 #include <locale.h>
 #include <stdio.h>
 #include <string.h>
+
+extern "C" {
+
+static char default_decimal_point[] = ".";
+static char default_thousands_sep[] = ",";
+static char default_grouping[] = "\x03\x03";
+
+static char default_empty_string[] = "";
+static char default_empty_value = 127;
+
+
+}
