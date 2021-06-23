a9c456b9cde6ff607a2c77ed22c0a08eaf1aac35
krishpranav <krisna.pranav@gmail.com>
2021-06-23 16:05:10 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/sched.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav, nuke123-sudo
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <errno.h>
+#include <sched.h>
+#include <syscall.h>
