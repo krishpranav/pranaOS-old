a4a04bdff503a30a71f52d0fe5c60577c5014dc8
krishpranav <krisna.pranav@gmail.com>
2021-06-23 12:13:36 +0530

    [+] setpwent [+]

--- UserLand/Libraries/LibC/pwd.cpp
+++ UserLand/Libraries/LibC/pwd.cpp
@@ -27,4 +27,17 @@ static String s_gecos;
 static String s_dir;
 static String s_shell;
 
+void setpwent()
+{
+    s_line_number = 0;
+    if (s_stream) {
+        rewind(s_stream);
+    } else {
+        s_stream = fopen("/etc/passwd", "r");
+        if (!s_stream) {
+            perror("open /etc/passwd");
+        }
+    }
+}
+
 }
