0dac503e028f409719bc341e5fd2bf0d7bc17eb0
evilbat831 <85536259+evilbat831@users.noreply.github.com>
2021-06-23 09:05:08 +0530

    pthread functions

--- UserLand/Libraries/LibC/pthread_forward.cpp
+++ UserLand/Libraries/LibC/pthread_forward.cpp
@@ -14,3 +14,33 @@ void __init_pthread_forward(PthreadFunctions funcs)
 {
   s_pthread_functions = funcs;
 }
+
+int pthread_mutex_trylock(pthread_mutex_t* mutex)
+{
+    VERIFY(s_pthread_functions.pthread_mutex_trylock);
+    return s_pthread_functions.pthread_mutex_trylock(mutex);
+}
+
+int pthread_mutex_destroy(pthread_mutex_t* mutex)
+{
+    VERIFY(s_pthread_functions.pthread_mutex_destroy);
+    return s_pthread_functions.pthread_mutex_destroy(mutex);
+}
+
+int pthread_mutexattr_init(pthread_mutexattr_t* attr)
+{
+    VERIFY(s_pthread_functions.pthread_mutexattr_init);
+    return s_pthread_functions.pthread_mutexattr_init(attr);
+}
+
+int pthread_mutexattr_settype(pthread_mutexattr_t* attr, int type)
+{
+    VERIFY(s_pthread_functions.pthread_mutexattr_settype);
+    return s_pthread_functions.pthread_mutexattr_settype(attr, type);
+}
+
+int pthread_mutexattr_destroy(pthread_mutexattr_t* attr)
+{
+    VERIFY(s_pthread_functions.pthread_mutexattr_destroy);
+    return s_pthread_functions.pthread_mutexattr_destroy(attr);
+}
