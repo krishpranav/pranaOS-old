865bc637f085c6d5925e1b7752aed454ea6cbf34
krishpranav <krisna.pranav@gmail.com>
2021-06-23 10:37:40 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/pthread_tls.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <AK/Atomic.h>
+#include <LibPthread/pthread.h>
+#include <errno.h>
+#include <unistd.h>
