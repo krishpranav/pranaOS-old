bcd871a16f734caa4801dc64cb116eb480111e26
krishpranav <krisna.pranav@gmail.com>
2021-06-23 12:25:27 +0530

    [+] public classes [+]

--- UserLand/Libraries/LibC/qsort.cpp
+++ UserLand/Libraries/LibC/qsort.cpp
@@ -9,3 +9,15 @@
 #include <AK/QuickSort.h>
 #include <stdlib.h>
 #include <sys/types.h>
+
+class SizedObject {
+public:
+    SizedObject() = delete;
+    SizedObject(void* data, size_t size)
+        : m_data(data)
+        , m_size(size)
+    {
+    }
+    void* data() const { return m_data; }
+    size_t size() const { return m_size; }
+};
