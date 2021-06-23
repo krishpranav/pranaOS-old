20413c62b69184b94d955dbac9da29990b9ecdb5
krishpranav <krisna.pranav@gmail.com>
2021-06-23 12:22:21 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/qsort.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <AK/Assertions.h>
+#include <AK/QuickSort.h>
+#include <stdlib.h>
+#include <sys/types.h>
