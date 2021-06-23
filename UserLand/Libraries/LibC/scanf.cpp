d41918ff15b8007f07490e4042728d2fe26c0ea1
krishpranav <krisna.pranav@gmail.com>
2021-06-23 15:11:58 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/scanf.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav, nuke123-sudo
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <AK/Assertions.h>
+#include <AK/Format.h>
+#include <AK/GenericLexer.h>
+#include <AK/StdLibExtras.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
