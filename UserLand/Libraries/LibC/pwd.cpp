77e4b85e041a90789f734c82efbd783550bc91b9
krishpranav <krisna.pranav@gmail.com>
2021-06-23 12:11:19 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/pwd.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <AK/String.h>
+#include <AK/TemporaryChange.h>
+#include <AK/Vector.h>
+#include <errno.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
