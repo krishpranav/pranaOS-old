5e421c0013cf1fe043b3b035631bf0943a100c15
krishpranav <krisna.pranav@gmail.com>
2021-06-23 08:20:52 +0530

    [+] init pthread forward [+]

--- UserLand/Libraries/LibC/pthread_forward.cpp
+++ UserLand/Libraries/LibC/pthread_forward.cpp
@@ -7,3 +7,10 @@
 // includes
 #include <AK/Assertions.h>
 #include <LibC/bits/pthread_forward.h>
+
+static PthreadFunctions s_pthread_functions;
+
+void __init_pthread_forward(PthreadFunctions funcs)
+{
+  s_pthread_functions = funcs;
+}
