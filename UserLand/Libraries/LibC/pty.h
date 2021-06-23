36e1e42f5bd2c593b5f60509d79e4595f2990832
krishpranav <krisna.pranav@gmail.com>
2021-06-23 11:29:38 +0530

    [+] pty headers [+]

--- UserLand/Libraries/LibC/pty.h
+++ UserLand/Libraries/LibC/pty.h
@@ -6,12 +6,14 @@
 
 #pragma once
 
-// includes 
+// includes
 #include <sys/cdefs.h>
 #include <termios.h>
 
 __BEGIN_DECLS
 
 int openpty(int* amaster, int* aslave, char* name, const struct termios* termp, const struct winsize* winp);
+pid_t forkpty(int* amaster, char* name, const struct termios* termp, const struct winsize* winp);
 int login_tty(int fd);
+
 __END_DECLS
