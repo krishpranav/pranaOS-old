28a8f66a004858d2aea75a232dc30e6e4d35c205
krishpranav <krisna.pranav@gmail.com>
2021-06-23 11:27:22 +0530

    [+] pthread tls [+]

--- UserLand/Libraries/LibC/pthread_tls.cpp
+++ UserLand/Libraries/LibC/pthread_tls.cpp
@@ -69,6 +69,39 @@ void* __pthread_getspecific(pthread_key_t key)
 
 void* pthread_getspecific(pthread_key_t) __attribute__((weak, alias("__pthread_getspecific")));
 
+int __pthread_setspecific(pthread_key_t key, const void* value)
+{
+    if (key < 0)
+        return EINVAL;
+    if (key >= max_keys)
+        return EINVAL;
 
+    t_specifics.values[key] = const_cast<void*>(value);
+    return 0;
+}
+
+int pthread_setspecific(pthread_key_t, const void*) __attribute__((weak, alias("__pthread_setspecific")));
+
+void __pthread_key_destroy_for_current_thread()
+{
+
+    __pthread_mutex_lock(&s_keys.mutex);
+    size_t num_used_keys = s_keys.next;
+
+    for (size_t destruct_iteration = 0; destruct_iteration < PTHREAD_DESTRUCTOR_ITERATIONS; ++destruct_iteration) {
+        bool any_nonnull_destructors = false;
+        for (size_t key_index = 0; key_index < num_used_keys; ++key_index) {
+            void* value = exchange(t_specifics.values[key_index], nullptr);
+
+            if (value && s_keys.destructors[key_index]) {
+                any_nonnull_destructors = true;
+                (*s_keys.destructors[key_index])(value);
+            }
+        }
+        if (!any_nonnull_destructors)
+            break;
+    }
+    __pthread_mutex_unlock(&s_keys.mutex);
+}
 }
 #endif
