70b0bcfdc5ae552ce402e898be316813da747cb5
krishpranav <krisna.pranav@gmail.com>
2021-06-24 11:48:15 +0530

    [+] parse shadow entry [+]

--- UserLand/Libraries/LibC/shadow.cpp
+++ UserLand/Libraries/LibC/shadow.cpp
@@ -88,6 +88,65 @@ static bool parse_shadow_entry(const String& line)
         return false;
     }
 
+    if (min_string.is_empty())
+        min_string = "-1";
+    auto min_value = min_string.to_int();
+    if (!min_value.has_value()) {
+        dbgln("getspent(): Malformed min value on line {}", s_line_number);
+        return false;
+    }
+
+    if (max_string.is_empty())
+        max_string = "-1";
+    auto max_value = max_string.to_int();
+    if (!max_value.has_value()) {
+        dbgln("getspent(): Malformed max value on line {}", s_line_number);
+        return false;
+    }
+
+    if (warn_string.is_empty())
+        warn_string = "-1";
+    auto warn = warn_string.to_int();
+    if (!warn.has_value()) {
+        dbgln("getspent(): Malformed warn on line {}", s_line_number);
+        return false;
+    }
+
+    if (inact_string.is_empty())
+        inact_string = "-1";
+    auto inact = inact_string.to_int();
+    if (!inact.has_value()) {
+        dbgln("getspent(): Malformed inact on line {}", s_line_number);
+        return false;
+    }
+
+    if (expire_string.is_empty())
+        expire_string = "-1";
+    auto expire = expire_string.to_int();
+    if (!expire.has_value()) {
+        dbgln("getspent(): Malformed expire on line {}", s_line_number);
+        return false;
+    }
+
+    if (flag_string.is_empty())
+        flag_string = "0";
+    auto flag = flag_string.to_int();
+    if (!flag.has_value()) {
+        dbgln("getspent(): Malformed flag on line {}", s_line_number);
+        return false;
+    }
+
+    s_shadow_entry.sp_namp = const_cast<char*>(s_name.characters());
+    s_shadow_entry.sp_pwdp = const_cast<char*>(s_pwdp.characters());
+    s_shadow_entry.sp_lstchg = lstchg.value();
+    s_shadow_entry.sp_min = min_value.value();
+    s_shadow_entry.sp_max = max_value.value();
+    s_shadow_entry.sp_warn = warn.value();
+    s_shadow_entry.sp_inact = inact.value();
+    s_shadow_entry.sp_expire = expire.value();
+    s_shadow_entry.sp_flag = flag.value();
+
+    return true;
 }
 
 }
