2d9d69523fd3c5b80206b80f52c48af30f3e7763
krishpranav <krisna.pranav@gmail.com>
2021-06-24 13:08:58 +0530

    [+] signal [+]

--- UserLand/Libraries/LibC/signal.cpp
+++ UserLand/Libraries/LibC/signal.cpp
@@ -30,6 +30,7 @@ int killpg(int pgrp, int sig)
 
 int raise(int sig)
 {
+    // FIXME: Support multi-threaded programs.
     return kill(getpid(), sig);
 }
 
@@ -52,4 +53,177 @@ int sigaction(int signum, const struct sigaction* act, struct sigaction* old_act
     __RETURN_WITH_ERRNO(rc, rc, -1);
 }
 
+int sigemptyset(sigset_t* set)
+{
+    *set = 0;
+    return 0;
+}
+
+int sigfillset(sigset_t* set)
+{
+    *set = 0xffffffff;
+    return 0;
+}
+
+int sigaddset(sigset_t* set, int sig)
+{
+    if (sig < 1 || sig > 32) {
+        errno = EINVAL;
+        return -1;
+    }
+    *set |= 1 << (sig - 1);
+    return 0;
+}
+
+int sigdelset(sigset_t* set, int sig)
+{
+    if (sig < 1 || sig > 32) {
+        errno = EINVAL;
+        return -1;
+    }
+    *set &= ~(1 << (sig - 1));
+    return 0;
+}
+
+int sigismember(const sigset_t* set, int sig)
+{
+    if (sig < 1 || sig > 32) {
+        errno = EINVAL;
+        return -1;
+    }
+    if (*set & (1 << (sig - 1)))
+        return 1;
+    return 0;
+}
+
+int sigprocmask(int how, const sigset_t* set, sigset_t* old_set)
+{
+    int rc = syscall(SC_sigprocmask, how, set, old_set);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+int sigpending(sigset_t* set)
+{
+    int rc = syscall(SC_sigpending, set);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+const char* sys_siglist[NSIG] = {
+    "Invalid signal number",
+    "Hangup",
+    "Interrupt",
+    "Quit",
+    "Illegal instruction",
+    "Trap",
+    "Aborted",
+    "Bus error",
+    "Division by zero",
+    "Killed",
+    "User signal 1",
+    "Segmentation violation",
+    "User signal 2",
+    "Broken pipe",
+    "Alarm clock",
+    "Terminated",
+    "Stack fault",
+    "Child exited",
+    "Continued",
+    "Stopped (signal)",
+    "Stopped",
+    "Stopped (tty input)",
+    "Stopped (tty output)",
+    "Urgent I/O condition)",
+    "CPU limit exceeded",
+    "File size limit exceeded",
+    "Virtual timer expired",
+    "Profiling timer expired",
+    "Window changed",
+    "I/O possible",
+    "Power failure",
+    "Bad system call",
+};
+
+int sigsetjmp(jmp_buf env, int savesigs)
+{
+    if (savesigs) {
+        int rc = sigprocmask(0, nullptr, &env->saved_signal_mask);
+        assert(rc == 0);
+        env->did_save_signal_mask = true;
+    } else {
+        env->did_save_signal_mask = false;
+    }
+    return setjmp(env);
+}
+void siglongjmp(jmp_buf env, int val)
+{
+    if (env->did_save_signal_mask) {
+        int rc = sigprocmask(SIG_SETMASK, &env->saved_signal_mask, nullptr);
+        assert(rc == 0);
+    }
+    longjmp(env, val);
+}
+
+int sigsuspend(const sigset_t*)
+{
+    dbgln("FIXME: Implement sigsuspend()");
+    return -1;
+}
+
+static const char* signal_names[] = {
+    "INVAL",
+    "HUP",
+    "INT",
+    "QUIT",
+    "ILL",
+    "TRAP",
+    "ABRT",
+    "BUS",
+    "FPE",
+    "KILL",
+    "USR1",
+    "SEGV",
+    "USR2",
+    "PIPE",
+    "ALRM",
+    "TERM",
+    "STKFLT",
+    "CHLD",
+    "CONT",
+    "STOP",
+    "TSTP",
+    "TTIN",
+    "TTOU",
+    "URG",
+    "XCPU",
+    "XFSZ",
+    "VTALRM",
+    "PROF",
+    "WINCH",
+    "IO",
+    "INFO",
+    "SYS",
+};
+
+static_assert(sizeof(signal_names) == sizeof(const char*) * NSIG);
+
+int getsignalbyname(const char* name)
+{
+    VERIFY(name);
+    for (size_t i = 0; i < NSIG; ++i) {
+        auto* signal_name = signal_names[i];
+        if (!strcmp(signal_name, name))
+            return i;
+    }
+    errno = EINVAL;
+    return -1;
+}
+
+const char* getsignalname(int signal)
+{
+    if (signal < 0 || signal >= NSIG) {
+        errno = EINVAL;
+        return nullptr;
+    }
+    return signal_names[signal];
+}
 }
