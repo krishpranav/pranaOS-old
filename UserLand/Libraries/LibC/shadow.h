03157dc1190868e51e83937c60e3f50a14e74a45
krishpranav <krisna.pranav@gmail.com>
2021-06-24 11:38:35 +0530

    [+] AK OS MACOS [+]

---
+++ UserLand/Libraries/LibC/shadow.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021 nuke123-sudo
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+
+#pragma once
+
+#ifndef AK_OS_MACOS
+#    include <bits/FILE.h>
+#endif
+#include <sys/cdefs.h>
+#include <sys/types.h>
