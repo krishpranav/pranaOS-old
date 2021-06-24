7a8ba9eb20a0e95d2b0f6d88d944ae33c8764a08
krishpranav <krisna.pranav@gmail.com>
2021-06-24 11:57:42 +0530

    [+] int [+]

--- UserLand/Libraries/LibC/pranaos.h
+++ UserLand/Libraries/LibC/pranaos.h
@@ -10,3 +10,12 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+__BEGIN_DECLS
+
+int disown(pid_t);
+
+int module_load(const char* path, size_t path_lenght);
+int module_unload(const char* name, size_t name_lenght);
+
+__END_DECLS
