0d54a737a11e1dc5b8ed2fcb5e243e1f2aa39bdd
krishpranav <krisna.pranav@gmail.com>
2021-06-24 17:01:50 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/stat.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021 krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <syscall.h>
+#include <unistd.h>
