ff3092772c04a28dcd0f81a420b19c2c983c717f
krishpranav <krisna.pranav@gmail.com>
2021-06-24 17:15:24 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/stdio.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021 krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#define _STDIO_H 
+
+// includes
+#include <bits/FILE.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <sys/cdefs.h>
+#include <sys/types.h>
