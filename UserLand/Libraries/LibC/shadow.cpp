7d03063fbb3dcbd3f4c0eaa4f5598164b6515b66
krishpranav <krisna.pranav@gmail.com>
2021-06-24 11:43:23 +0530

    [+] set spent [+]

--- UserLand/Libraries/LibC/shadow.cpp
+++ UserLand/Libraries/LibC/shadow.cpp
@@ -25,4 +25,17 @@ static struct spwd s_shadow_entry;
 static String s_name;
 static String s_pwdp;
 
+void setspent()
+{
+    s_line_number = 0;
+    if (s_stream) {
+        rewind(s_stream);
+    } else {
+        s_stream = fopen("/etc/shadow", "r");
+        if (!s_stream) {
+            dbgln("open /etc/shadow failed: {}", strerror(errno));
+        }
+    }
+}
+
 }
