7cf307d939278aabed4d7373f1a647531dba6c0a
krishpranav <krisna.pranav@gmail.com>
2021-06-24 12:03:24 +0530

    [+] module unload [+]

--- UserLand/Libraries/LibC/pranaos.cpp
+++ UserLand/Libraries/LibC/pranaos.cpp
@@ -26,4 +26,11 @@ int module_load(const char* path, size_t path_length)
     __RETURN_WITH_ERRNO(rc, rc, -1);
 }
 
+int module_unload(const char* name, size_t name_length)
+{
+    int rc = syscall(SC_module_unload, name, name_length);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+
 }
