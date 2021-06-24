99afe3baa61e385a4bf27f76e9aeb60e3a981804
krishpranav <krisna.pranav@gmail.com>
2021-06-24 15:16:18 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/spawn.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <spawn.h>
+#include <AK/Function.h>
+#include <AK/Vector.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
