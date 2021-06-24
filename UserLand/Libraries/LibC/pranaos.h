a693b0bfe73fa9cb340d8cd90ff831b080b47cb7
krishpranav <krisna.pranav@gmail.com>
2021-06-24 11:56:47 +0530

    [+] update [+]

---
+++ UserLand/Libraries/LibC/pranaos.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 nuke123-sudo
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
