9066113aebb9877699d3a9440357f6e597d9518c
krishpranav <krisna.pranav@gmail.com>
2021-06-24 16:11:56 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/ssp.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021 krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <AK/Format.h>
+#include <AK/Types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/internals.h>
+#include <unistd.h>
