88c1681543b8fc03c3d15edf338cb6e037c89209
krishpranav <krisna.pranav@gmail.com>
2021-06-24 11:41:42 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/shadow.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 nuke123-sudo
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+
+// includes
+#include <AK/String.h>
+#include <AK/TemporaryChange.h>
+#include <AK/Vector.h>
+#include <errno.h>
+#include <shadow.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
