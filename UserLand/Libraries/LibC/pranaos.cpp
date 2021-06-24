8bc40704d22bef30941f513ec6813f4c7e8be255
krishpranav <krisna.pranav@gmail.com>
2021-06-24 12:02:48 +0530

    [+] diswon [+]

--- UserLand/Libraries/LibC/pranaos.cpp
+++ UserLand/Libraries/LibC/pranaos.cpp
@@ -10,3 +10,14 @@
 #include <serenity.h>
 #include <string.h>
 #include <syscall.h>
+
+
+extern "C" {
+
+int disown(pid_t pid)
+{
+    int rc = syscall(SC_disown, pid);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+}
