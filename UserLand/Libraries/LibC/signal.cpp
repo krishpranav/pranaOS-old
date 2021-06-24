e6d8b52d83efea4cff8e414e440ab05216bff5fd
krishpranav <krisna.pranav@gmail.com>
2021-06-24 13:07:20 +0530

    [+] sighandler [+]

--- UserLand/Libraries/LibC/signal.cpp
+++ UserLand/Libraries/LibC/signal.cpp
@@ -33,4 +33,17 @@ int raise(int sig)
     return kill(getpid(), sig);
 }
 
+sighandler_t signal(int signum, sighandler_t handler)
+{
+    struct sigaction new_act;
+    struct sigaction old_act;
+    new_act.sa_handler = handler;
+    new_act.sa_flags = 0;
+    new_act.sa_mask = 0;
+    int rc = sigaction(signum, &new_act, &old_act);
+    if (rc < 0)
+        return SIG_ERR;
+    return old_act.sa_handler;
+}
+
 }
