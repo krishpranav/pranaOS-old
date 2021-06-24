43e3434dd8d0185b909b0daaa2991310bc0c9f5b
krishpranav <krisna.pranav@gmail.com>
2021-06-24 12:02:22 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/pranaos.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 nuke123-sudo
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <arpa/inet.h>
+#include <errno.h>
+#include <serenity.h>
+#include <string.h>
+#include <syscall.h>
