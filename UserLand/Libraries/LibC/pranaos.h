46a28a762e60b47970f8c4d5a344c2d72292c553
krishpranav <krisna.pranav@gmail.com>
2021-06-24 11:58:58 +0530

    [+] profiling values [+]

--- UserLand/Libraries/LibC/pranaos.h
+++ UserLand/Libraries/LibC/pranaos.h
@@ -18,4 +18,8 @@ int disown(pid_t);
 int module_load(const char* path, size_t path_lenght);
 int module_unload(const char* name, size_t name_lenght);
 
+int profiling_enable(pid_t, uint64_t);
+int profiling_disable(pid_t);
+int profiling_free_buffer(pid_t);
+
 __END_DECLS
