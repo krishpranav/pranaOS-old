c629dbe43eb156e2fe640204d9fae0ae29cd6d75
krishpranav <krisna.pranav@gmail.com>
2021-06-24 15:18:34 +0530

    [+] noreturn posix spawn child [+]

--- UserLand/Libraries/LibC/spawn.h
+++ UserLand/Libraries/LibC/spawn.h
@@ -22,5 +22,69 @@ struct posix_spawn_file_actions_state {
 
 [[noreturn]] static void posix_spawn_child(const char* path, const posix_spawn_file_actions_t* file_actions, const posix_spawnattr_t* attr, char* const argv[], char* const envp[], int (*exec)(const char*, char* const[], char* const[]))
 {
-    
+    if (attr) {
+        short flags = attr->flags;
+        if (flags & POSIX_SPAWN_RESETIDS) {
+            if (seteuid(getuid()) < 0) {
+                perror("posix_spawn seteuid");
+                _exit(127);
+            }
+            if (setegid(getgid()) < 0) {
+                perror("posix_spawn setegid");
+                _exit(127);
+            }
+        }
+        if (flags & POSIX_SPAWN_SETPGROUP) {
+            if (setpgid(0, attr->pgroup) < 0) {
+                perror("posix_spawn setpgid");
+                _exit(127);
+            }
+        }
+        if (flags & POSIX_SPAWN_SETSCHEDPARAM) {
+            if (sched_setparam(0, &attr->schedparam) < 0) {
+                perror("posix_spawn sched_setparam");
+                _exit(127);
+            }
+        }
+        if (flags & POSIX_SPAWN_SETSIGDEF) {
+            struct sigaction default_action;
+            default_action.sa_flags = 0;
+            sigemptyset(&default_action.sa_mask);
+            default_action.sa_handler = SIG_DFL;
+
+            sigset_t sigdefault = attr->sigdefault;
+            for (int i = 0; i < NSIG; ++i) {
+                if (sigismember(&sigdefault, i) && sigaction(i, &default_action, nullptr) < 0) {
+                    perror("posix_spawn sigaction");
+                    _exit(127);
+                }
+            }
+        }
+        if (flags & POSIX_SPAWN_SETSIGMASK) {
+            if (sigprocmask(SIG_SETMASK, &attr->sigmask, nullptr) < 0) {
+                perror("posix_spawn sigprocmask");
+                _exit(127);
+            }
+        }
+        if (flags & POSIX_SPAWN_SETSID) {
+            if (setsid() < 0) {
+                perror("posix_spawn setsid");
+                _exit(127);
+            }
+        }
+
+    }
+
+    if (file_actions) {
+        for (const auto& action : file_actions->state->actions) {
+            if (action() < 0) {
+                perror("posix_spawn file action");
+                _exit(127);
+            }
+        }
+    }
+
+    exec(path, argv, envp);
+    perror("posix_spawn exec");
+    _exit(127);
 }
