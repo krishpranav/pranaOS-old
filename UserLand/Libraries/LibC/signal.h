7bc3e5df3d36ea9a661afd77b601020453f7b964
krishpranav <krisna.pranav@gmail.com>
2021-06-24 13:00:44 +0530

    [+] siginfo [+]

--- UserLand/Libraries/LibC/signal.h
+++ UserLand/Libraries/LibC/signal.h
@@ -24,4 +24,14 @@ union sigval {
     void* sival_ptr;
 };
 
+typedef struct siginfo {
+    int si_signo;
+    int si_code;
+    pid_t si_pid;
+    uid_t si_uid;
+    void* si_addr;
+    int si_status;
+    union sigval si_value;
+} siginfo_t;
+
 __END_DECLS
