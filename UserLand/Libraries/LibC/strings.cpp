ee6b19650dc65234edd25c5020b67e5ff49009c4
krishpranav <krisna.pranav@gmail.com>
2021-06-25 13:24:24 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/strings.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <assert.h>
+#include <ctype.h>
+#include <string.h>
+#include <strings.h>
+
