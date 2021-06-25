d4e77349c3135d75ac2897645c1ccf7bd3962f91
krishpranav <krisna.pranav@gmail.com>
2021-06-25 12:50:25 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/stdlib.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <stddef.h>
+#include <sys/cdefs.h>
+#include <sys/types.h>
