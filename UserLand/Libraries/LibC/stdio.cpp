f87392e4784b450882d7586a8db32cf4d91ca963
krishpranav <krisna.pranav@gmail.com>
2021-06-25 10:41:51 +0530

    [+] stdio FILE [+ ]

--- UserLand/Libraries/LibC/stdio.cpp
+++ UserLand/Libraries/LibC/stdio.cpp
@@ -27,6 +27,7 @@
 #include <unistd.h>
 
 struct FILE {
+public:
     FILE(int fd, int mode)
         : m_fd(fd)
         , m_mode(mode)
@@ -73,14 +74,12 @@ struct FILE {
 
 private:
     struct Buffer {
-        // A ringbuffer that also transparently implements ungetc().
     public:
         ~Buffer();
 
         int mode() const { return m_mode; }
         void setbuf(u8* data, int mode, size_t size);
-        // Make sure to call realize() before enqueuing any data.
-        // Dequeuing can be attempted without it.
+
         void realize(int fd);
         void drop();
 
@@ -96,4 +95,36 @@ private:
 
         bool enqueue_front(u8 byte);
 
-}
+    private:
+        u8* m_data { nullptr };
+        size_t m_capacity { BUFSIZ };
+        size_t m_begin { 0 };
+        size_t m_end { 0 };
+
+        int m_mode { -1 };
+        u8 m_unget_buffer { 0 };
+        bool m_ungotten : 1 { false };
+        bool m_data_is_malloced : 1 { false };
+        bool m_empty : 1 { true };
+    };
+
+    ssize_t do_read(u8*, size_t);
+    ssize_t do_write(const u8*, size_t);
+
+    bool read_into_buffer();
+    bool write_from_buffer();
+
+    void lock();
+    void unlock();
+
+    int m_fd { -1 };
+    int m_mode { 0 };
+    u8 m_flags { Flags::None };
+    int m_error { 0 };
+    bool m_eof { false };
+    pid_t m_popen_child { -1 };
+    Buffer m_buffer;
+    __pthread_mutex_t m_mutex;
+
+    friend class ScopedFileLock;
+};
