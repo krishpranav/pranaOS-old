432b72a80269bda79512b8d8b05403077575a52d
krishpranav <krisna.pranav@gmail.com>
2021-06-25 13:18:15 +0530

    [+ ]attribute [+]

--- UserLand/Libraries/LibC/string.h
+++ UserLand/Libraries/LibC/string.h
@@ -28,4 +28,12 @@ void* memset(void*, int, size_t);
 void explicit_bzero(void*, size_t) __attribute__((nonnull(1)));
 
 
+__attribute__((malloc)) char* strdup(const char*);
+__attribute__((malloc)) char* strndup(const char*, size_t);
+
+__attribute__((deprecated("use strlcpy or String::copy_characters_to_buffer"))) char* strcpy(char* dest, const char* src);
+__attribute__((deprecated("use strlcpy or String::copy_characters_to_buffer"))) char* strncpy(char* dest, const char* src, size_t);
+__attribute__((warn_unused_result)) size_t strlcpy(char* dest, const char* src, size_t);
+
+
 __END_DECLS
