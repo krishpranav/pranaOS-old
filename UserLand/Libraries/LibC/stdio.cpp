e32a7cbd7b1d0b1997cf0ecc7fdb1a2fccc466f7
krishpranav <krisna.pranav@gmail.com>
2021-06-25 10:37:03 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/stdio.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021 krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+
+// includes
+#include <AK/Format.h>
+#include <AK/PrintfImplementation.h>
+#include <AK/ScopedValueRollback.h>
+#include <AK/StdLibExtras.h>
+#include <AK/String.h>
+#include <LibC/bits/pthread_integration.h>
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/internals.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <syscall.h>
+#include <unistd.h>
