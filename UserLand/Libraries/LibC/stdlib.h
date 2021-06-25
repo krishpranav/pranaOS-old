f4c0bd17ef5fd77af9d4b815e1b3a1e34266bcfc
krishpranav <krisna.pranav@gmail.com>
2021-06-25 12:52:08 +0530

    [+] define, attributes [+]

--- UserLand/Libraries/LibC/stdlib.h
+++ UserLand/Libraries/LibC/stdlib.h
@@ -13,3 +13,13 @@
 
 __attribute__((warn_unused_result)) int __generate_unique_filename(char* pattern);
 
+__BEGIN_DECLS
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+#define MB_CUR_MAX 1
+
+__attribute__((malloc)) __attribute__((alloc_size(1))) void* malloc(size_t);
+__attribute__((malloc)) __attribute__((alloc_size(1, 2))) void* calloc(size_t nmemb, size_t);
+
+__END_DECLS
