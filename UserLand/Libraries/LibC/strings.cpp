b43b95c178a975b6291efa24f696a72eac4c2f28
krishpranav <krisna.pranav@gmail.com>
2021-06-25 13:25:47 +0530

    [+] strings [+]

--- UserLand/Libraries/LibC/strings.cpp
+++ UserLand/Libraries/LibC/strings.cpp
@@ -10,9 +10,8 @@
 #include <string.h>
 #include <strings.h>
 
-
 extern "C" {
-    
+
 void bzero(void* dest, size_t n)
 {
     memset(dest, 0, n);
@@ -23,12 +22,32 @@ void bcopy(const void* src, void* dest, size_t n)
     memmove(dest, src, n);
 }
 
-static char foldcaes(char ch)
+static char foldcase(char ch)
 {
     if (isalpha(ch))
         return tolower(ch);
-    
     return ch;
 }
 
+int strcasecmp(const char* s1, const char* s2)
+{
+    for (; foldcase(*s1) == foldcase(*s2); ++s1, ++s2) {
+        if (*s1 == 0)
+            return 0;
+    }
+    return foldcase(*(const unsigned char*)s1) < foldcase(*(const unsigned char*)s2) ? -1 : 1;
+}
+
+int strncasecmp(const char* s1, const char* s2, size_t n)
+{
+    if (!n)
+        return 0;
+    do {
+        if (foldcase(*s1) != foldcase(*s2++))
+            return foldcase(*(const unsigned char*)s1) - foldcase(*(const unsigned char*)--s2);
+        if (*s1++ == 0)
+            break;
+    } while (--n);
+    return 0;
+}
 }
