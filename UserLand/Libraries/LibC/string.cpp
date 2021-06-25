111cfd07cbe757b91f8ee0e316cecff34ce5f8c0
krishpranav <krisna.pranav@gmail.com>
2021-06-25 13:18:51 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/string.cpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <AK/Format.h>
+#include <AK/MemMem.h>
+#include <AK/Platform.h>
+#include <AK/StdLibExtras.h>
+#include <AK/Types.h>
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
