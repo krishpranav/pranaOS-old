d82432dc4f0bf0b3c6bfa6ac7642cc69b750e7d6
krishpranav <krisna.pranav@gmail.com>
2021-06-25 13:22:14 +0530

    [+] char [+]

--- UserLand/Libraries/LibC/string.cpp
+++ UserLand/Libraries/LibC/string.cpp
@@ -181,4 +181,22 @@ const void* memmem(const void* haystack, size_t haystack_length, const void* nee
     return AK::memmem(haystack, haystack_length, needle, needle_length);
 }
 
+char* strcpy(char* dest, const char* src)
+{
+    char* original_dest = dest;
+    while ((*dest++ = *src++) != '\0')
+        ;
+    return original_dest;
+}
+
+char* strncpy(char* dest, const char* src, size_t n)
+{
+    size_t i;
+    for (i = 0; i < n && src[i] != '\0'; ++i)
+        dest[i] = src[i];
+    for (; i < n; ++i)
+        dest[i] = '\0';
+    return dest;
+}
+
 }
