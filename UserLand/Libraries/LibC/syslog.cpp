ea21fe7892b838700d021ef8f1b167e60b569dbd
krishpranav <krisna.pranav@gmail.com>
2021-06-27 15:58:16 +0530

    [+] get syslog ident [+]

--- UserLand/Libraries/LibC/syslog.cpp
+++ UserLand/Libraries/LibC/syslog.cpp
@@ -27,5 +27,17 @@ static struct syslog_data global_log_data = {
 static char program_name_buffer[256];
 static bool program_name_set = false;
 
+static const char* get_syslog_ident(struct syslog_data* data)
+{
+    if (!program_name_set && data->ident == nullptr)
+        program_name_set = get_process_name(program_name_buffer, sizeof(program_name_buffer)) >= 0;
+
+    if (data->ident != nullptr)
+        return data->ident;
+    else if (program_name_set)
+        return program_name_buffer;
+
+    VERIFY_NOT_REACHED();
+}
 
 }
