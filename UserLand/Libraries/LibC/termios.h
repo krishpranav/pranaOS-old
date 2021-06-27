8508df3960c61d4010426be6ff47e2dd222a59b0
krishpranav <krisna.pranav@gmail.com>
2021-06-27 16:16:40 +0530

    [+] termios [+]

---
+++ UserLand/Libraries/LibC/termios.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 krishpranav, nuke123-sudo
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+
+#pragma once
+
+// includes
+#include <sys/cdefs.h>
+#include <sys/types.h>
+
+__BEGIN_DECLS
+
+
+__END_DECLS
