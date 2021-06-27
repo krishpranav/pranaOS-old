22d77abe7db3be5b905e034d8b017aa88943a3a5
krishpranav <krisna.pranav@gmail.com>
2021-06-27 16:02:55 +0530

    [+] termcap headers [+]

--- UserLand/Libraries/LibC/termcap.h
+++ UserLand/Libraries/LibC/termcap.h
@@ -15,4 +15,11 @@ extern char PC;
 extern char* UP;
 extern char* BC;
 
+int tgetent(char* bp, const char* name);
+int tgetflag(const char* id);
+int tgetnum(const char* id);
+char* tgetstr(const char* id, char** area);
+char* tgoto(const char* cap, int col, int row);
+int tputs(const char* str, int affcnt, int (*putc)(int));
+
 __END_DECLS
