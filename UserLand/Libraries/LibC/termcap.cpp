cd8393eb5714203631bfd4a634ebec1e04e94dec
krishpranav <krisna.pranav@gmail.com>
2021-06-27 16:03:38 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/termcap.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021 krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <AK/Debug.h>
+#include <AK/HashMap.h>
+#include <AK/String.h>
+#include <AK/Vector.h>
+#include <assert.h>
+#include <string.h>
+#include <termcap.h>
