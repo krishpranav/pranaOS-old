c1199b43f66f36f1bcbb49f7c8097ad5fcd63f57
krishpranav <krisna.pranav@gmail.com>
2021-06-28 13:06:53 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/arpa/inet.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <endian.h>
+#include <inttypes.h>
+#include <netinet/in.h>
+#include <sys/cdefs.h>
+#include <sys/socket.h>
