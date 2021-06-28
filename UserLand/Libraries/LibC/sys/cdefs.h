bc8a0d9cf09997e1368dd6289e62311284eadaa2
krishpranav <krisna.pranav@gmail.com>
2021-06-28 15:06:30 +0530

    [+] cplus cplus [+]

--- UserLand/Libraries/LibC/sys/cdefs.h
+++ UserLand/Libraries/LibC/sys/cdefs.h
@@ -11,3 +11,15 @@
 #ifndef ALWAYS_INLINE
 #    define ALWAYS_INLINE inline __attribute__((always_inline))
 #endif
+
+#ifdef __cplusplus
+#    ifndef __BEGIN_DECLS
+#        define __BEGIN_DECLS extern "C" {
+#        define __END_DECLS }
+#    endif
+#else
+#    ifndef __BEGIN_DECLS
+#        define __BEGIN_DECLS
+#        define __END_DECLS
+#    endif
+#endif
