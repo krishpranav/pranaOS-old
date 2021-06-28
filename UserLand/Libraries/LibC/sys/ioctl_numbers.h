a9dc1feb4c7986a0a0ad4812d3281ea8f3e64b67
krishpranav <krisna.pranav@gmail.com>
2021-06-28 15:12:00 +0530

    [+] struct values [+]

--- UserLand/Libraries/LibC/sys/ioctl_numbers.h
+++ UserLand/Libraries/LibC/sys/ioctl_numbers.h
@@ -11,7 +11,6 @@
 
 __BEGIN_DECLS
 
-
 struct winsize {
     unsigned short ws_row;
     unsigned short ws_col;
@@ -19,6 +18,22 @@ struct winsize {
     unsigned short ws_ypixel;
 };
 
+struct FBResolution {
+    unsigned pitch;
+    unsigned width;
+    unsigned height;
+};
 
+struct FBRect {
+    unsigned x;
+    unsigned y;
+    unsigned width;
+    unsigned height;
+};
+
+struct FBRects {
+    unsigned count;
+    FBRect const* rects;
+};
 
 __END_DECLS
