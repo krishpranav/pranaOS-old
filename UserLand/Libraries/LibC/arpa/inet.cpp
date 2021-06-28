000cc60c98384c9828bb9f8f05a39cf6478006e0
krishpranav <krisna.pranav@gmail.com>
2021-06-28 13:11:23 +0530

    [+] inet ntop [+]

--- UserLand/Libraries/LibC/arpa/inet.cpp
+++ UserLand/Libraries/LibC/arpa/inet.cpp
@@ -8,3 +8,22 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <stdio.h>
+
+extern "C" {
+
+const char* inet_ntop(int af, const void* src, char* dst, socklen_t len)
+{
+    if (af != AF_INET) {
+        errno = EAFNOSUPPORT;
+        return nullptr;
+    }
+    if (len < 4) {
+        errno = ENOSPC;
+        return nullptr;
+    }
+    auto* bytes = (const unsigned char*)src;
+    snprintf(dst, len, "%u.%u.%u.%u", bytes[0], bytes[1], bytes[2], bytes[3]);
+    return (const char*)dst;
+}
+
+}
