5d058d39740032b0a25bf74b2acecf152e591a32
krishpranav <krisna.pranav@gmail.com>
2021-06-28 13:09:51 +0530

    [+] inet [+]

--- UserLand/Libraries/LibC/arpa/inet.h
+++ UserLand/Libraries/LibC/arpa/inet.h
@@ -26,4 +26,34 @@ static inline int inet_aton(const char* cp, struct in_addr* inp)
     return inet_pton(AF_INET, cp, inp);
 }
 
+char* inet_ntoa(struct in_addr);
+
+static inline uint16_t htons(uint16_t value)
+{
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    return __builtin_bswap16(value);
+#else
+    return value;
+#endif
+}
+
+static inline uint16_t ntohs(uint16_t value)
+{
+    return htons(value);
+}
+
+static inline uint32_t htonl(uint32_t value)
+{
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    return __builtin_bswap32(value);
+#else
+    return value;
+#endif
+}
+
+static inline uint32_t ntohl(uint32_t value)
+{
+    return htonl(value);
+}
+
 __END_DECLS
