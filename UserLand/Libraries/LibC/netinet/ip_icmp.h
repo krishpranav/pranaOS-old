15ca40742899248228e0a37bfc01f81f82e76d18
krishpranav <krisna.pranav@gmail.com>
2021-06-28 14:55:35 +0530

    [+] icmphdr [+]

--- UserLand/Libraries/LibC/netinet/ip_icmp.h
+++ UserLand/Libraries/LibC/netinet/ip_icmp.h
@@ -12,4 +12,17 @@
 
 __BEGIN_DECLS 
 
+struct icmphdr {
+    uint8_t type;
+    uint8_t code;
+    uint16_t checksum;
+    union {
+        struct {
+            uint16_t id;
+            uint16_t sequence;
+        } echo;
+        uint32_t gateway;
+    } un;
+};
+
 __END_DECLS 
