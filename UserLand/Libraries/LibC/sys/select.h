647c76dbeb0deee525c46666ebdefcb68e9f0745
krishpranav <krisna.pranav@gmail.com>
2021-06-28 15:47:00 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/sys/select.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <fd_set.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/cdefs.h>
+#include <sys/time.h>
+#include <sys/types.h>
