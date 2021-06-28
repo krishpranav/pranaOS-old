fd824a0627dfe65cf873bd03df9af37eab416d4b
krishpranav <krisna.pranav@gmail.com>
2021-06-28 11:59:23 +0530

    [+] ulimit [+]

--- UserLand/Libraries/LibC/ulimit.cpp
+++ UserLand/Libraries/LibC/ulimit.cpp
@@ -12,12 +12,28 @@
 
 extern "C" {
 
-
 long ulimit([[maybe_unused]] int cmd, [[maybe_unused]] long newlimit)
 {
     dbgln("FIXME: Implement getrusage()");
     TODO();
     return -1;
-};
+}
 
+int getrusage([[maybe_unused]] int who, [[maybe_unused]] struct rusage* usage)
+{
+    dbgln("FIXME: Implement getrusage()");
+    return -1;
+}
+
+int getrlimit([[maybe_unused]] int resource, rlimit* rl)
+{
+    rl->rlim_cur = RLIM_INFINITY;
+    rl->rlim_max = RLIM_INFINITY;
+    return 0;
+}
+
+int setrlimit([[maybe_unused]] int resource, [[maybe_unused]] rlimit const* rl)
+{
+    return 0;
+}
 }
