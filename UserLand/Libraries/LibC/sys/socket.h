4d5c78f86e68426ea3d7377d36a0d889bd4affaf
krishpranav <krisna.pranav@gmail.com>
2021-06-28 15:51:09 +0530

    [+] define values [+]

--- UserLand/Libraries/LibC/sys/socket.h
+++ UserLand/Libraries/LibC/sys/socket.h
@@ -11,3 +11,42 @@
 #include <sys/cdefs.h>
 #include <sys/types.h>
 #include <sys/un.h>
+
+__BEGIN_DECLS
+
+#define AF_MASK 0xff
+#define AF_UNSPEC 0
+#define AF_LOCAL 1
+#define AF_UNIX AF_LOCAL
+#define AF_INET 2
+#define AF_INET6 3
+#define AF_MAX 4
+#define PF_LOCAL AF_LOCAL
+#define PF_UNIX PF_LOCAL
+#define PF_INET AF_INET
+#define PF_INET6 AF_INET6
+#define PF_UNSPEC AF_UNSPEC
+#define PF_MAX AF_MAX
+
+#define SOCK_TYPE_MASK 0xff
+#define SOCK_STREAM 1
+#define SOCK_DGRAM 2
+#define SOCK_RAW 3
+#define SOCK_NONBLOCK 04000
+#define SOCK_CLOEXEC 02000000
+
+#define SHUT_RD 1
+#define SHUT_WR 2
+#define SHUT_RDWR 3
+
+#define IPPROTO_IP 0
+#define IPPROTO_ICMP 1
+#define IPPROTO_TCP 6
+#define IPPROTO_UDP 17
+#define IPPROTO_IPV6 41
+
+#define MSG_TRUNC 0x1
+#define MSG_CTRUNC 0x2
+#define MSG_PEEK 0x4
+#define MSG_OOB 0x8
+#define MSG_DONTWAIT 0x40
