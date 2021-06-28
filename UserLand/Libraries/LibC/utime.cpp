3a7363f87af359419afe56efbb6c117e888d07af
krishpranav <krisna.pranav@gmail.com>
2021-06-28 12:46:51 +0530

    [+] utime [+]

--- UserLand/Libraries/LibC/utime.cpp
+++ UserLand/Libraries/LibC/utime.cpp
@@ -11,5 +11,14 @@
 #include <utime.h>
 
 extern "C" {
-    
+
+int utime(const char* pathname, const struct utimbuf* buf)
+{
+    if (!pathname) {
+        errno = EFAULT;
+        return -1;
+    }
+    int rc = syscall(SC_utime, pathname, strlen(pathname), buf);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
 }
