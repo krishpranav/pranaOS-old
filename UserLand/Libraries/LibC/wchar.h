a191b7236fcc0856499a57fb365e19909af8f701
krishpranav <krisna.pranav@gmail.com>
2021-06-28 12:56:16 +0530

    [+] wchar [+]

--- UserLand/Libraries/LibC/wchar.h
+++ UserLand/Libraries/LibC/wchar.h
@@ -19,4 +19,18 @@ __BEGIN_DECLS
 typedef __WINT_TYPE__ wint_t;
 typedef unsigned long int wctype_t;
 
+size_t wcslen(const wchar_t*);
+wchar_t* wcscpy(wchar_t*, const wchar_t*);
+wchar_t* wcsncpy(wchar_t*, const wchar_t*, size_t);
+int wcscmp(const wchar_t*, const wchar_t*);
+int wcsncmp(const wchar_t*, const wchar_t*, size_t);
+wchar_t* wcschr(const wchar_t*, int);
+const wchar_t* wcsrchr(const wchar_t*, wchar_t);
+wchar_t* wcscat(wchar_t*, const wchar_t*);
+wchar_t* wcsncat(wchar_t*, const wchar_t*, size_t);
+wchar_t* wcstok(wchar_t*, const wchar_t*, wchar_t**);
+long wcstol(const wchar_t*, wchar_t**, int);
+long long wcstoll(const wchar_t*, wchar_t**, int);
+wint_t btowc(int c);
+
 __END_DECLS
