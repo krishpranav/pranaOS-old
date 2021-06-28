fff9fd8bfb720f8458941436e0855bf2730763ac
krishpranav <krisna.pranav@gmail.com>
2021-06-28 15:06:15 +0530

    [+] define [+]

---
+++ UserLand/Libraries/LibC/sys/cdefs.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#define _POSIX_VERSION 200809L
+
+#ifndef ALWAYS_INLINE
+#    define ALWAYS_INLINE inline __attribute__((always_inline))
+#endif
