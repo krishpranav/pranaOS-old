ea8130967d1f5cc18a04d55e36d652c20424b8a1
krishpranav <krisna.pranav@gmail.com>
2021-06-28 12:52:24 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/wchar.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021 krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <stddef.h>
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+__END_DECLS
