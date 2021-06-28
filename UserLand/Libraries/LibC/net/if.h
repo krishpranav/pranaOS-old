daa4f6b090bbaec3a07e5477a51930f4b862ed6e
krishpranav <krisna.pranav@gmail.com>
2021-06-28 13:27:26 +0530

    [+] struct [+]

--- UserLand/Libraries/LibC/net/if.h
+++ UserLand/Libraries/LibC/net/if.h
@@ -12,5 +12,22 @@
 
 __BEGIN_DECLS
 
+struct ifreq {
+#define IFNAMSIZ 16
+    char ifr_name[IFNAMSIZ];
+    union {
+        struct sockaddr ifru_addr;
+        struct sockaddr ifru_dstaddr;
+        struct sockaddr ifru_broadaddr;
+        struct sockaddr ifru_netmask;
+        struct sockaddr ifru_hwaddr;
+        short ifru_flags;
+        int ifru_metric;
+        int64_t ifru_vnetid;
+        uint64_t ifru_media;
+        void* ifru_data;
+        unsigned int ifru_index;
+    } ifr_ifru;
+
 
 __END_DECLS
