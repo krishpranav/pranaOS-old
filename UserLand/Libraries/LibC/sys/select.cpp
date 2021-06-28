789502ed753bd8d7d796b09fb12798e8dd3c5374
krishpranav <krisna.pranav@gmail.com>
2021-06-28 15:48:25 +0530

    [+] select [+]

--- UserLand/Libraries/LibC/sys/select.cpp
+++ UserLand/Libraries/LibC/sys/select.cpp
@@ -10,3 +10,19 @@
 #include <sys/select.h>
 #include <sys/time.h>
 #include <syscall.h>
+
+extern "C" {
+
+
+int select(int nfds, fd_set* readfds, fd_set* writefds, fd_set* exceptfds, timeval* timeout_tv)
+{
+    timespec* timeout_ts = nullptr;
+    timespec timeout;
+    if (timeout_tv) {
+        timeout_ts = &timeout;
+        TIMEVAL_TO_TIMESPEC(timeout_tv, timeout_ts);
+    }
+    return pselect(nfds, readfds, writefds, exceptfds, timeout_ts, nullptr);
+}
+
+}
