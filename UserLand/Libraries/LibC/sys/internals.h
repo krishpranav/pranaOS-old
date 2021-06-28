5514b5196272227a089c29a02cdd2dc4ee8bce98
krishpranav <krisna.pranav@gmail.com>
2021-06-28 15:07:51 +0530

    [+] internal func [+]

---
+++ UserLand/Libraries/LibC/sys/internals.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+typedef void (*AtExitFunction)(void*);
+
+
+__END_DECLS
