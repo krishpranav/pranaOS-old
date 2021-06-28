e073266bbdd604b47aeb8da6a9c9830cf2ba30f3
krishpranav <krisna.pranav@gmail.com>
2021-06-28 12:57:47 +0530

    [+] wcslen [+]

--- UserLand/Libraries/LibC/wchar.cpp
+++ UserLand/Libraries/LibC/wchar.cpp
@@ -8,3 +8,14 @@
 #include <AK/Assertions.h>
 #include <AK/Format.h>
 #include <wchar.h>
+
+extern "C" {
+
+size_t wcslen(const wchar_t* str)
+{
+    size_t len = 0;
+    while (*(str++))
+        ++len;
+    return len;
+}
+}
