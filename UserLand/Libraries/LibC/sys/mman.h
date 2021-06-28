55b5d41870572ccd27a550ec6781dbf90acc3cc4
krishpranav <krisna.pranav@gmail.com>
2021-06-28 15:13:26 +0530

    [+] define map [+]

--- UserLand/Libraries/LibC/sys/mman.h
+++ UserLand/Libraries/LibC/sys/mman.h
@@ -10,3 +10,13 @@
 // includes
 #include <sys/cdefs.h>
 #include <sys/types.h>
+
+#define MAP_FILE 0x00
+#define MAP_SHARED 0x01
+#define MAP_PRIVATE 0x02
+#define MAP_FIXED 0x10
+#define MAP_ANONYMOUS 0x20
+#define MAP_ANON MAP_ANONYMOUS
+#define MAP_STACK 0x40
+#define MAP_NORESERVE 0x80
+#define MAP_RANDOMIZED 0x100
