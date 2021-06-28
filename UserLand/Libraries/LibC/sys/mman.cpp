ad6a0978d7ba0a358b3f42ee58baa7f6f3ac11d1
krishpranav <krisna.pranav@gmail.com>
2021-06-28 15:14:52 +0530

    [+] pranaos mmap [+]

--- UserLand/Libraries/LibC/sys/mman.cpp
+++ UserLand/Libraries/LibC/sys/mman.cpp
@@ -11,3 +11,18 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <syscall.h>
+
+extern "C" {
+
+void* pranaos_mmap(void* addr, size_t size, int prot, int flags, int fd, off_t offset, size_t alignment, const char* name)
+{
+    Syscall::SC_mmap_params params { (uintptr_t)addr, size, alignment, prot, flags, fd, offset, { name, name ? strlen(name) : 0 } };
+    ptrdiff_t rc = syscall(SC_mmap, &params);
+    if (rc < 0 && -rc < EMAXERRNO) {
+        errno = -rc;
+        return MAP_FAILED;
+    }
+    return (void*)rc;
+}
+
+}
