6adb3898155cd9d887b23a4f336840dffeae2956
krishpranav <krisna.pranav@gmail.com>
2021-06-28 15:47:38 +0530

    [+] select headers [+]

--- UserLand/Libraries/LibC/sys/select.h
+++ UserLand/Libraries/LibC/sys/select.h
@@ -13,3 +13,10 @@
 #include <sys/cdefs.h>
 #include <sys/time.h>
 #include <sys/types.h>
+
+__BEGIN_DECLS
+
+int select(int nfds, fd_set* readfds, fd_set* writefds, fd_set* exceptfds, struct timeval* timeout);
+int pselect(int nfds, fd_set* readfds, fd_set* writefds, fd_set* exceptfds, const struct timespec* timeout, const sigset_t* sigmask);
+
+__END_DECLS
