4cdf4d4e79c596238adfac3c68c8436bbffe32b0
krishpranav <krisna.pranav@gmail.com>
2021-06-28 14:51:24 +0530

    [+] define [+]

--- UserLand/Libraries/LibC/netinet/in.h
+++ UserLand/Libraries/LibC/netinet/in.h
@@ -18,4 +18,17 @@ __BEGIN_DECLS
 typedef uint32_t in_addr_t;
 in_addr_t inet_addr(const char*);
 
+#define INADDR_ANY ((in_addr_t)0)
+#define INADDR_NONE ((in_addr_t)-1)
+#define INADDR_LOOPBACK 0x7f000001
+
+#define IN_LOOPBACKNET 127
+
+#define IP_TTL 2
+#define IP_MULTICAST_LOOP 3
+#define IP_ADD_MEMBERSHIP 4
+#define IP_DROP_MEMBERSHIP 5
+#define IP_MULTICAST_IF 6
+#define IP_MULTICAST_TTL 7
+
 __END_DECLS
