2ba480b270bc3b03a41d2ee08cc8a8d43af5985b
krishpranav <krisna.pranav@gmail.com>
2021-06-28 15:16:07 +0530

    [+] mman [+]

--- UserLand/Libraries/LibC/sys/mman.cpp
+++ UserLand/Libraries/LibC/sys/mman.cpp
@@ -35,4 +35,53 @@ void* mmap_with_name(void* addr, size_t size, int prot, int flags, int fd, off_t
     return pranaos_mmap(addr, size, prot, flags, fd, offset, PAGE_SIZE, name);
 }
 
+void* mremap(void* old_address, size_t old_size, size_t new_size, int flags)
+{
+    Syscall::SC_mremap_params params { (uintptr_t)old_address, old_size, new_size, flags };
+    ptrdiff_t rc = syscall(SC_mremap, &params);
+    if (rc < 0 && -rc < EMAXERRNO) {
+        errno = -rc;
+        return MAP_FAILED;
+    }
+    return (void*)rc;
+}
+
+int munmap(void* addr, size_t size)
+{
+    int rc = syscall(SC_munmap, addr, size);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+int mprotect(void* addr, size_t size, int prot)
+{
+    int rc = syscall(SC_mprotect, addr, size, prot);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+int set_mmap_name(void* addr, size_t size, const char* name)
+{
+    if (!name) {
+        errno = EFAULT;
+        return -1;
+    }
+    Syscall::SC_set_mmap_name_params params { addr, size, { name, strlen(name) } };
+    int rc = syscall(SC_set_mmap_name, &params);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+int madvise(void* address, size_t size, int advice)
+{
+    int rc = syscall(SC_madvise, address, size, advice);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+void* allocate_tls(const char* initial_data, size_t size)
+{
+    ptrdiff_t rc = syscall(SC_allocate_tls, initial_data, size);
+    if (rc < 0 && -rc < EMAXERRNO) {
+        errno = -rc;
+        return MAP_FAILED;
+    }
+    return (void*)rc;
+}
 }
