7365653ed9bedb5594d73ef878e4bf988525eb65
krishpranav <krisna.pranav@gmail.com>
2021-06-28 11:29:27 +0530

    includes

---
+++ UserLand/Libraries/LibC/time.cpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 krishpranav, nuke123-sudo
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <AK/String.h>
+#include <AK/StringBuilder.h>
+#include <AK/Time.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/times.h>
+#include <syscall.h>
+#include <time.h>
+#include <utime.h>
