4d0cb1fe3adc2abc4a1edc765facd4fbae8bc41c
krishpranav <krisna.pranav@gmail.com>
2021-06-28 13:10:16 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/arpa/inet.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
