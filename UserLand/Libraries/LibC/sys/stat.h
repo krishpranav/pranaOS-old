aa553ce2911a5f277ca6029699b708a860498320
krishpranav <krisna.pranav@gmail.com>
2021-06-29 09:10:06 +0530

    [+] define s [+]

--- UserLand/Libraries/LibC/sys/stat.h
+++ UserLand/Libraries/LibC/sys/stat.h
@@ -10,3 +10,17 @@
 #include <sys/cdefs.h>
 #include <sys/types.h>
 #include <time.h>
+
+__BEGIN_DECLS
+
+#define S_IFMT 0170000
+#define S_IFDIR 0040000
+#define S_IFCHR 0020000
+#define S_IFBLK 0060000
+#define S_IFREG 0100000
+#define S_IFIFO 0010000
+#define S_IFLNK 0120000
+#define S_IFSOCK 0140000
+
+
+__END_DECLS
