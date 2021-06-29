d52cd378c19b4f5b640e668805d7ce083ea96a9e
krishpranav <krisna.pranav@gmail.com>
2021-06-29 11:01:34 +0530

    [+] card stack [+]

--- UserLand/Libraries/LibCards/CardStack.cpp
+++ UserLand/Libraries/LibCards/CardStack.cpp
@@ -6,3 +6,20 @@
 
 // includes
 #include "CardStack.h"
+
+CardStack::CardStack()
+    : m_position({ 0, 0 })
+    , m_type(Invalid)
+    , m_base(m_position, { Card::width, Card::height })
+{
+}
+
+CardStack::CardStack(const Gfx::IntPoint& position, Type type)
+    : m_position(position)
+    , m_type(type)
+    , m_rules(rules_for_type(type))
+    , m_base(m_position, { Card::width, Card::height })
+{
+    VERIFY(type != Invalid);
+    calculate_bounding_box();
+}
