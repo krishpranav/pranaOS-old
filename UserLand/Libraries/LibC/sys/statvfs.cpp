a279aad500dc574c2dac057d4783e1a5183edb8f
krishpranav <krisna.pranav@gmail.com>
2021-06-29 09:14:58 +0530

    [+] statvfs [+]

--- UserLand/Libraries/LibC/sys/statvfs.cpp
+++ UserLand/Libraries/LibC/sys/statvfs.cpp
@@ -10,3 +10,14 @@
 #include <string.h>
 #include <sys/statvfs.h>
 #include <syscall.h>
+
+extern "C" {
+    
+int statvfs(const char* path, struct statvfs* buf)
+{
+    Syscall::SC_statvfs_params params { { path, strlen(path) }, buf };
+    int rc = syscall(SC_statvfs, &params);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+}
