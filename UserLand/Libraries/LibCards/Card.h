58d800412b18a806ba5123095db577b6fdb7330c
krishpranav <krisna.pranav@gmail.com>
2021-06-29 10:33:21 +0530

    [+] public [+]

--- UserLand/Libraries/LibCards/Card.h
+++ UserLand/Libraries/LibCards/Card.h
@@ -15,3 +15,20 @@
 #include <LibGfx/CharacterBitmap.h>
 #include <LibGfx/Rect.h>
 #include <ctype.h>
+
+
+namespace Cards {
+
+class Card final : public Core::Object {
+    C_OBJECT(Card)
+public:
+    static constexpr int width = 80;
+    static constexpr int height = 100;
+    static constexpr int card_count = 13;
+    static constexpr int card_radius = 5;
+    static constexpr Array<StringView, card_count> labels = {
+        "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"
+    };
+
+
+}
