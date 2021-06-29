ecc5468125817944f0ef5026b628a9e5fbb3caf8
krishpranav <krisna.pranav@gmail.com>
2021-06-29 10:59:04 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibCards/Card.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include "Card.h"
+#include <LibGUI/Widget.h>
+#include <LibGfx/Font.h>
+#include <LibGfx/FontDatabase.h>
