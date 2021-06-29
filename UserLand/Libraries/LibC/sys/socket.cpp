4bea2c7d8f1268be5f926ecf4acd61571aba51ef
krishpranav <krisna.pranav@gmail.com>
2021-06-29 08:58:02 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/sys/socket.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <AK/Assertions.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+#include <syscall.h>
