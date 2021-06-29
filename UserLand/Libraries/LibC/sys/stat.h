d3e2b64c70ceea425c0fcf40e99dace4746b4f4d
krishpranav <krisna.pranav@gmail.com>
2021-06-29 09:09:33 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibC/sys/stat.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once 
+
+// includes
+#include <sys/cdefs.h>
+#include <sys/types.h>
+#include <time.h>
