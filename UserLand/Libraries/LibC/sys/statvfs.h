c02737883e02d2038697a8357e179025e0a4cbac
krishpranav <krisna.pranav@gmail.com>
2021-06-29 09:13:30 +0530

    [+] statvfs struct [+]

--- UserLand/Libraries/LibC/sys/statvfs.h
+++ UserLand/Libraries/LibC/sys/statvfs.h
@@ -16,4 +16,21 @@ __BEGIN_DECLS
 #define ST_RDONLY 0x1
 #define ST_NOSUID 0x2
 
+struct statvfs {
+    unsigned long f_bsize;
+    unsigned long f_frsize;
+    fsblkcnt_t f_blocks;
+    fsblkcnt_t f_bfree;
+    fsblkcnt_t f_bavail;
+
+    fsfilcnt_t f_files;
+    fsfilcnt_t f_ffree;
+    fsfilcnt_t f_favail;
+
+    unsigned long f_fsid;
+    unsigned long f_flag;
+    unsigned long f_namemax;
+};
+
+
 __END_DECLS
