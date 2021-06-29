08e843ce25cbd4ff5e363a7d95a17b3092bbc427
krishpranav <krisna.pranav@gmail.com>
2021-06-29 09:10:54 +0530

    [+] stat struct [+]

--- UserLand/Libraries/LibC/sys/stat.h
+++ UserLand/Libraries/LibC/sys/stat.h
@@ -50,5 +50,21 @@ __BEGIN_DECLS
 #define S_ISLNK(m) (((m)&S_IFMT) == S_IFLNK)
 #define S_ISSOCK(m) (((m)&S_IFMT) == S_IFSOCK)
 
+struct stat {
+    dev_t st_dev;           
+    ino_t st_ino;           
+    mode_t st_mode;         
+    nlink_t st_nlink;       
+    uid_t st_uid;           
+    gid_t st_gid;           
+    dev_t st_rdev;           
+    off_t st_size;           
+    blksize_t st_blksize;    
+    blkcnt_t st_blocks;      
+    struct timespec st_atim; 
+    struct timespec st_mtim; 
+    struct timespec st_ctim; 
+};
+
 
 __END_DECLS
