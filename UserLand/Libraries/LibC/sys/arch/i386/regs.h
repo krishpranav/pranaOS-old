bfd780e417775a6f805176ec1df86f51751cf6e0
krishpranav <krisna.pranav@gmail.com>
2021-06-29 10:27:22 +0530

    [+] regs i386 [+]

--- UserLand/Libraries/LibC/sys/arch/i386/regs.h
+++ UserLand/Libraries/LibC/sys/arch/i386/regs.h
@@ -9,3 +9,22 @@
 
 // includes
 #include <AK/Types.h>
+
+struct [[gnu::packed]] PtraceRegisters {
+    u32 eax;
+    u32 ecx;
+    u32 edx;
+    u32 ebx;
+    u32 esp;
+    u32 ebp;
+    u32 esi;
+    u32 edi;
+    u32 eip;
+    u32 eflags;
+    u32 cs;
+    u32 ss;
+    u32 ds;
+    u32 es;
+    u32 fs;
+    u32 gs;
+};
