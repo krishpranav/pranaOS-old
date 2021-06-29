b283b714b603d1a2a3a8cbeda8bb60cc88e9f663
krishpranav <krisna.pranav@gmail.com>
2021-06-29 10:59:24 +0530

    [+] Nonnul ref [+]

--- UserLand/Libraries/LibCards/Card.cpp
+++ UserLand/Libraries/LibCards/Card.cpp
@@ -9,3 +9,20 @@
 #include <LibGUI/Widget.h>
 #include <LibGfx/Font.h>
 #include <LibGfx/FontDatabase.h>
+
+namespace Cards {
+
+static const NonnullRefPtr<Gfx::CharacterBitmap> s_diamond = Gfx::CharacterBitmap::create_from_ascii(
+    "    #    "
+    "   ###   "
+    "  #####  "
+    " ####### "
+    "#########"
+    " ####### "
+    "  #####  "
+    "   ###   "
+    "    #    ",
+    9, 9);
+
+
+}
