582139a421a0dd5d998805c6d76c63eccf9101c4
krishpranav <krisna.pranav@gmail.com>
2021-06-29 10:31:39 +0530

    [+] includes [+]

---
+++ UserLand/Libraries/LibCards/Card.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once 
+
+// includes
+#include <AK/Array.h>
+#include <AK/Format.h>
+#include <LibCore/Object.h>
+#include <LibGUI/Painter.h>
+#include <LibGfx/Bitmap.h>
+#include <LibGfx/CharacterBitmap.h>
+#include <LibGfx/Rect.h>
+#include <ctype.h>
