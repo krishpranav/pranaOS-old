980a0e5de591506b660c16173de66d1f93c16528
krishpranav <krisna.pranav@gmail.com>
2021-06-29 09:00:45 +0530

    [+] bind, listen socket functions [+]

--- UserLand/Libraries/LibC/sys/socket.cpp
+++ UserLand/Libraries/LibC/sys/socket.cpp
@@ -21,4 +21,17 @@ int socket(int domain, int type, int protocol)
     __RETURN_WITH_ERRNO(rc, rc, -1);
 }
 
+int bind(int sockfd, const sockaddr* addr, socklen_t addrlen)
+{
+    int rc = syscall(SC_bind, sockfd, addr, addrlen);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+int listen(int sockfd, int backlog)
+{
+    int rc = syscall(SC_listen, sockfd, backlog);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+
 }
